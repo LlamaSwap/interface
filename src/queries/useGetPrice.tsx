1d41e6590f386f099ec0ebc82b225610adb8aebc
mintdart <96025197+mintdart@users.noreply.github.com>
2022-11-18 17:15:54 +0100

    update refetchInterval

--- src/queries/useGetPrice.tsx
+++ src/queries/useGetPrice.tsx
@@ -31,6 +31,6 @@ async function getPrice({ chain, fromToken, toToken }: IGetPriceProps) {
 
 export default function useGetPrice({ chain, fromToken, toToken }: IGetPriceProps) {
 	return useQuery<IPrice>(['gasPrice', chain, fromToken, toToken], () => getPrice({ chain, fromToken, toToken }), {
-		refetchInterval: 30_000
+		refetchInterval: 20_000
 	});
 }
