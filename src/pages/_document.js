4d6da8b9e643daeb45b2e1f31faabefed0153ce7
vrtnd <vrotend@proton.me>
2022-12-05 22:13:33 +0300

    Revert

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -2,27 +2,26 @@ import Document, { Html, Head, Main, NextScript } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
 
 class MyDocument extends Document {
-	// static async getInitialProps(ctx) {
-	//   const sheet = new ServerStyleSheet();
-	//   const originalRenderPage = ctx.renderPage;
+	static async getInitialProps(ctx) {
+		const sheet = new ServerStyleSheet();
+		const originalRenderPage = ctx.renderPage;
 
-	//   try {
-	//     ctx.renderPage = () =>
-	//       originalRenderPage({
-	//         enhanceApp: (App) => (props) =>
-	//           sheet.collectStyles(<App {...props} />),
-	//       });
+		try {
+			ctx.renderPage = () =>
+				originalRenderPage({
+					enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />)
+				});
 
-	//     const initialProps = await Document.getInitialProps(ctx);
+			const initialProps = await Document.getInitialProps(ctx);
 
-	//     return {
-	//       ...initialProps,
-	//       styles: [initialProps.styles, sheet.getStyleElement()],
-	//     };
-	//   } finally {
-	//     sheet.seal();
-	//   }
-	// }
+			return {
+				...initialProps,
+				styles: [initialProps.styles, sheet.getStyleElement()]
+			};
+		} finally {
+			sheet.seal();
+		}
+	}
 
 	render() {
 		return (
