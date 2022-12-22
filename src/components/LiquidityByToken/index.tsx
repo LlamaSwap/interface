14fd330a70a9214b0e48796a06a9032504beef82
mintdart <96025197+mintdart@users.noreply.github.com>
2022-12-22 09:23:53 +0100

    add charts

--- src/components/LiquidityByToken/index.tsx
+++ src/components/LiquidityByToken/index.tsx
@@ -1,3 +1,4 @@
+import * as React from 'react';
 import styled from 'styled-components';
 import BigNumber from 'bignumber.js';
 import { Box, Flex, Skeleton } from '@chakra-ui/react';
@@ -5,12 +6,20 @@ import { ArrowRight } from 'react-feather';
 import { liquidity } from '~/components/Aggregator/constants';
 import type { IToken } from '~/types';
 import { useGetTokenLiquidity } from '~/queries/useGetTokenLiquidity';
-import { useMemo } from 'react';
+import dynamic from 'next/dynamic';
+
+interface ISlippageChart {
+	chartData: Array<[number, number]>;
+	fromTokenSymbol: string;
+	toTokenSymbol: string;
+}
+
+const SlippageChart = dynamic(() => import('../SlippageChart'), { ssr: false }) as React.FC<ISlippageChart>;
 
 export function LiquidityByToken({ fromToken, toToken, chain }: { fromToken: IToken; toToken: IToken; chain: string }) {
 	const { data, isLoading } = useGetTokenLiquidity({ fromToken, toToken, chain });
 
-	const { topRoutes, chartData } = useMemo(() => {
+	const { topRoutes, chartData } = React.useMemo(() => {
 		const topRoutes =
 			data?.map(([liq, routes]) => {
 				const sortedRoutes = routes.sort(
@@ -49,72 +58,86 @@ export function LiquidityByToken({ fromToken, toToken, chain }: { fromToken: ITo
 
 					const slippage = ((Number(amountReturned) - expectedPrice) / expectedPrice) * 100;
 
-					chartData.push([nofOfTokensToSwap, Number(Math.abs(slippage).toFixed(2))]);
+					chartData.push([
+						nofOfTokensToSwap,
+						Number(Math.abs(slippage).toFixed(2)),
+						BigNumber(amountReturned)
+							.div(10 ** Number(toToken.decimals || 18))
+							.toFixed(3)
+					]);
 				}
 			});
 		}
 
 		return { topRoutes, chartData };
-	}, [data]);
+	}, [data, toToken.decimals]);
 
 	return (
-		<Table>
-			<caption>
-				<Flex alignItems="center" flexWrap="nowrap" justifyContent="center" gap={1}>
-					<Flex as="span" alignItems="center" gap="2px" flexWrap="nowrap">
-						<img
-							src={fromToken.logoURI}
-							alt=""
-							style={{ width: '20px', height: '20px', objectFit: 'cover', display: ' flex' }}
-						/>
-						<Box as="span" fontWeight={500} fontSize={16}>
-							{fromToken.symbol}
-						</Box>
+		<Flex flexDir="column" gap="24px">
+			<Table>
+				<caption>
+					<Flex alignItems="center" flexWrap="nowrap" justifyContent="center" gap={1}>
+						<Flex as="span" alignItems="center" gap="2px" flexWrap="nowrap">
+							<img
+								src={fromToken.logoURI}
+								alt=""
+								style={{ width: '20px', height: '20px', objectFit: 'cover', display: ' flex', borderRadius: '100%' }}
+							/>
+							<Box as="span" fontWeight={500} fontSize={16}>
+								{fromToken.symbol}
+							</Box>
+						</Flex>
+						<ArrowRight width={16} height={16} display="block" />
+						<Flex as="span" alignItems="center" gap="2px" flexWrap="nowrap">
+							<img
+								src={toToken.logoURI}
+								alt=""
+								style={{ width: '20px', height: '20px', objectFit: 'cover', display: ' flex' }}
+							/>
+							<Box as="span" fontWeight={500} fontSize={16}>
+								{toToken.symbol}
+							</Box>
+						</Flex>
 					</Flex>
-					<ArrowRight width={16} height={16} display="block" />
-					<Flex as="span" alignItems="center" gap="2px" flexWrap="nowrap">
-						<img
-							src={toToken.logoURI}
-							alt=""
-							style={{ width: '20px', height: '20px', objectFit: 'cover', display: ' flex' }}
-						/>
-						<Box as="span" fontWeight={500} fontSize={16}>
-							{toToken.symbol}
-						</Box>
-					</Flex>
-				</Flex>
-			</caption>
-			<thead>
-				<tr>
-					<th>Trade</th>
-					<th>Receive</th>
-				</tr>
-			</thead>
-			<tbody>
-				{liquidity.slice(1).map((liq) => {
-					const topRoute = topRoutes.find((t) => t[0] === `${liq.amount}+${liq.slippage}`)?.[1] ?? null;
+				</caption>
+				<thead>
+					<tr>
+						<th>Trade</th>
+						<th>Receive</th>
+					</tr>
+				</thead>
+				<tbody>
+					{liquidity.slice(1).map((liq) => {
+						const topRoute = topRoutes.find((t) => t[0] === `${liq.amount}+${liq.slippage}`)?.[1] ?? null;
 
-					return (
-						<tr key={toToken.address + liq.amount + liq.slippage}>
-							<td>{`${liq.amount.toLocaleString()} ${fromToken.symbol}`}</td>
-							<td>
-								{isLoading ? (
-									<Skeleton height="16px" width="100%" maxWidth="24ch" margin="auto" />
-								) : (
-									<>
-										{topRoute?.price?.amountReturned
-											? `${BigNumber(topRoute?.price?.amountReturned ?? 0)
-													.div(10 ** Number(toToken.decimals || 18))
-													.toFixed(3)} ${toToken.symbol} via ${topRoute?.name}`
-											: ''}
-									</>
-								)}
-							</td>
-						</tr>
-					);
-				})}
-			</tbody>
-		</Table>
+						return (
+							<tr key={toToken.address + liq.amount + liq.slippage}>
+								<td>{`${liq.amount.toLocaleString()} ${fromToken.symbol}`}</td>
+								<td>
+									{isLoading ? (
+										<Skeleton height="16px" width="100%" maxWidth="24ch" margin="auto" />
+									) : (
+										<>
+											{topRoute?.price?.amountReturned
+												? `${BigNumber(topRoute?.price?.amountReturned ?? 0)
+														.div(10 ** Number(toToken.decimals || 18))
+														.toFixed(3)} ${toToken.symbol} via ${topRoute?.name}`
+												: ''}
+										</>
+									)}
+								</td>
+							</tr>
+						);
+					})}
+				</tbody>
+			</Table>
+
+			<Box height="400px">
+				{chartData.length > 0 && (
+					<SlippageChart chartData={chartData} fromTokenSymbol={fromToken.symbol} toTokenSymbol={toToken.symbol} />
+				)}{' '}
+			</Box>
+		</Flex>
 	);
 }
 
