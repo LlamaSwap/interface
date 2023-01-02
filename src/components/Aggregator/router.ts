3a509a3810b3b2f1a88b6f4198bf5ad1c9c10637
0xngmi <0xngmi@protonmail.com>
2023-01-02 20:34:03 +0000

    remove lifi

--- src/components/Aggregator/router.ts
+++ src/components/Aggregator/router.ts
@@ -17,7 +17,7 @@ import { allChains } from '../WalletProvider/chains';
 import { chainNamesReplaced, chainsMap } from './constants';
 // import * as krystal from './adapters/krystal'
 
-export const adapters = [matcha, inch, cowswap, kyberswap, openocean, yieldyak, lifi];
+export const adapters = [matcha, inch, cowswap, kyberswap, openocean, yieldyak];
 
 const adaptersMap = adapters.reduce((acc, adapter) => ({ ...acc, [adapter.name]: adapter }), {});
 
