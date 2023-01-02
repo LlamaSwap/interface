b9809eb28776177ddc343c5f55252facd4a6e56e
mintdart <96025197+mintdart@users.noreply.github.com>
2023-01-02 21:59:49 +0100

    add favicon

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -27,6 +27,10 @@ class MyDocument extends Document {
 		return (
 			<Html lang="en">
 				<Head>
+					<link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+					<link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+					<link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+					<link rel="manifest" href="/manifest.json" />
 					<link
 						href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@400;800&display=swap"
 						rel="stylesheet"
