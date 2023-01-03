2882b86504a65262f0d58b8f0860874568e576a3
mintdart <96025197+mintdart@users.noreply.github.com>
2023-01-03 12:26:55 +0100

    fix link to favicons

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -27,9 +27,9 @@ class MyDocument extends Document {
 		return (
 			<Html lang="en">
 				<Head>
-					<link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
-					<link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
-					<link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+					<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+					<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+					<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
 					<link rel="manifest" href="/manifest.json" />
 					<link
 						href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@400;800&display=swap"
