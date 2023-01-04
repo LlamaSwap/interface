0595f8c80a079b27904e3d3a7b1ccc31a47e37f7
mintdart <96025197+mintdart@users.noreply.github.com>
2023-01-04 17:39:35 +0100

    use icons cdn for chain icons

--- src/utils/index.tsx
+++ src/utils/index.tsx
@@ -1,7 +1,9 @@
 export const capitalizeFirstLetter = (word) => word.charAt(0).toUpperCase() + word.slice(1);
 
+const ICONS_CDN = 'https://icons.llamao.fi/icons';
+
 export function chainIconUrl(chain) {
-	return `/chain-icons/rsz_${chain.toLowerCase()}.jpg`;
+	return `${ICONS_CDN}/chains/rsz_${chain.toLowerCase()}?w=24&h=24`;
 }
 
 export function getSavedTokens() {
