09de22fe7ebf15c12444a6c79e3d2000f1470d2c
0xngmi <0xngmi@protonmail.com>
2023-01-17 11:53:54 +0000

    remove firebird

--- src/components/Aggregator/router.ts
+++ src/components/Aggregator/router.ts
@@ -17,7 +17,7 @@ import { allChains } from '../WalletProvider/chains';
 import { chainNamesReplaced, chainsMap } from './constants';
 // import * as krystal from './adapters/krystal'
 
-export const adapters = [matcha, inch, cowswap, firebird, kyberswap, openocean, yieldyak, paraswap];
+export const adapters = [matcha, inch, cowswap, kyberswap, openocean, yieldyak, paraswap];
 
 export const inifiniteApprovalAllowed = [matcha.name, inch.name, cowswap.name, kyberswap.name, paraswap.name];
 
