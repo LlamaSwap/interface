83a6182d91555c5f0ed92e2e0dc23801abe7d029
strobie <0xstrobe@protonmail.com>
2023-01-19 13:45:38 +0100

    kek

--- post-build.sh
+++ post-build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-rm ./next/server/pages/404.html
-echo "" >./next/server/pages/404.html
-rm ./next/server/pages/500.html
-echo "" >./next/server/pages/500.html
+rm ./.next/server/pages/404.html
+echo "" >./.next/server/pages/404.html
+rm ./.next/server/pages/500.html
+echo "" >./.next/server/pages/500.html
