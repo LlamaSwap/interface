73dcc569d1e161b6d12377b23da8cfb063d47036
vrtnd <vrotend@proton.me>
2023-01-20 17:33:25 +0100

    fix

--- src/components/Aggregator/adapters/yieldyak/index.ts
+++ src/components/Aggregator/adapters/yieldyak/index.ts
@@ -17,18 +17,44 @@ export function approvalAddress(chain: string) {
 
 const nativeToken = '0xB31f66AA3C1e785363F0875A1B74E27b85FD66c7';
 
-export async function getQuote(chain: string, from: string, to: string, amount: string) {
-	const routerContract = new ethers.Contract(chainToId[chain], ABI.yieldYakRouter, providers[chain]);
+export async function estimateGas({ chain, rawQuote, from, to, provider, userAddress }) {
+	let routerContract = new ethers.Contract(chainToId[chain], ABI.yieldYakRouter, provider).populateTransaction;
+
+	const swapFunc = (() => {
+		if (from === ethers.constants.AddressZero) return routerContract.swapNoSplitFromAVAX;
+		if (to === ethers.constants.AddressZero) return routerContract.swapNoSplitToAVAX;
+		return routerContract.swapNoSplit;
+	})();
+
+	const tx = await swapFunc(
+		[rawQuote.amounts[0], rawQuote.amounts[rawQuote.amounts.length - 1], rawQuote.path, rawQuote.adapters],
+		userAddress,
+		0,
+		from === ethers.constants.AddressZero ? { value: rawQuote.amounts[0], from: userAddress } : { from: userAddress }
+	);
+	const gas = await provider.estimateGas(tx);
+
+	return gas;
+}
+
+export async function getQuote(chain: string, from: string, to: string, amount: string, extra: any) {
+	const provider = providers[chain];
+	const routerContract = new ethers.Contract(chainToId[chain], ABI.yieldYakRouter, provider);
 	const tokenFrom = from === ethers.constants.AddressZero ? nativeToken : from;
 	const tokenTo = to === ethers.constants.AddressZero ? nativeToken : to;
-	const gasPrice = await providers[chain].getGasPrice();
+
+	const gasPrice = ethers.BigNumber.from(extra.gasPriceData.gasPrice);
+
 	const data = await routerContract.findBestPathWithGas(amount, tokenFrom, tokenTo, 3, gasPrice);
-
-	const gasEstimate = chain === 'optimism' ? BigNumber(3.5).times(data.gasEstimate).toFixed(0, 1) : data.gasEstimate;
-
+	let gas = data.gasEstimate;
+	try {
+		gas = await estimateGas({ chain, provider, rawQuote: data, from, to, userAddress: extra.userAddress });
+	} catch (e) {
+		console.log(e);
+	}
 	return {
 		amountReturned: data.amounts[data.amounts.length - 1].toString(),
-		estimatedGas: gasEstimate.toString(), // Gas estimates only include gas-cost of swapping and querying on adapter and not intermediate logic, nor tx-gas-cost.
+		estimatedGas: gas.toString(), // Gas estimates only include gas-cost of swapping and querying on adapter and not intermediate logic, nor tx-gas-cost.
 		rawQuote: data,
 		tokenApprovalAddress: '0xC4729E56b831d74bBc18797e0e17A295fA77488c',
 		logo: 'https://assets.coingecko.com/coins/images/17654/small/yieldyak.png?1665824438'
