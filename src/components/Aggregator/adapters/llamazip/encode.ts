ba8347194a18f4d941c5c40868f9f50d15cc0fe0
0xngmi <0xngmi@protonmail.com>
2023-02-10 03:35:25 +0000

    upgrade to v2

--- src/components/Aggregator/adapters/llamazip/encode.ts
+++ src/components/Aggregator/adapters/llamazip/encode.ts
@@ -15,21 +15,31 @@ function removeFirstBit(word:BigNumber){
   return word.toHexString().replace("0x01", "0x") // toHexString() normalizes to byte length, so we need to remove 2 nibbles
 }
 
-export function encode(pair:string, token0IsTokenIn:boolean, minReturnAmount:string, inputIsETH:boolean, maxBalance?:boolean, inputAmount?:string) {
+export function encode(pair:string, token0IsTokenIn:boolean, expectedReturnAmount:string, slippage:string, inputIsETH:boolean, maxBalance?:boolean, inputAmount?:string) {
   let word = BigNumber.from(1).shl(4).add(pair).shl(1)
   if (token0IsTokenIn) {
       word = word.add(1)
   }
-  word = word.shl(14)
+  word = word.shl(17)
   let slippageZeroes = 0;
-  let slippageNum = BigNumber.from(minReturnAmount)
-  while (slippageNum.gt(16383)) { // 0b11111111111111
+  let slippageNum = BigNumber.from(expectedReturnAmount)
+  while (slippageNum.gt(131071)) { // 0b11111111111111111
       slippageZeroes++;
       slippageNum = slippageNum.div(2)
   }
+  if(slippageNum.lt(131071)){
+    slippageNum = slippageNum.add(1) // round up
+  }
   word = word.add(slippageNum).shl(8).add(slippageZeroes)
+
+  const slippageId = ["0.5", "0.1", "1", "5"].findIndex(slip => slip === slippage)
+  if(slippageId === -1){
+    throw new Error("Slippage number not supported")
+  }
+  word = word.shl(2).add(slippageId)
+
   if (inputIsETH || maxBalance) {
-      return removeFirstBit(word.shl(5)) // pad it so total number of bits is a multiple of 8
+      return removeFirstBit(word) // pad it so total number of bits is a multiple of 8
   }
 
   let inputZeroes = 0;
@@ -39,7 +49,8 @@ export function encode(pair:string, token0IsTokenIn:boolean, minReturnAmount:str
       inputNum = inputNum.div(10);
   }
   word = word.shl(5).add(inputZeroes)
-  let inputBitlength = countBits(inputNum)
-  word = word.shl(inputBitlength + (8 - inputBitlength % 8))
+  const inputBitlength = countBits(inputNum)
+  const extraBits = inputBitlength % 8
+  word = word.shl(inputBitlength + (extraBits <= 3? 3-extraBits : 3 + 8 - extraBits))
   return removeFirstBit(word.add(inputNum))
 }
--- src/components/Aggregator/adapters/llamazip/index.ts
+++ src/components/Aggregator/adapters/llamazip/index.ts
@@ -7,7 +7,7 @@ export const name = 'LlamaZip';
 export const token = 'none';
 
 export const chainToId = {
-  optimism: '0xF09Ea2e82EbAE3c06ECB579473748d4f27371E72',
+  optimism: '0x1214687e3AA359EfF691d719971d81c9D8F0eB32',
 };
 
 // https://docs.uniswap.org/contracts/v3/reference/deployments
@@ -26,7 +26,32 @@ const pairs = {
       fee: "500",
       pairId: "0",
     },
+    ["0x4200000000000000000000000000000000000042".toLowerCase()]: {
+      fee: "3000",
+      pairId: "1",
+    },
+    // pool 3 is ignored because we already have one with same tokens
+    ["0x8700dAec35aF8Ff88c16BdF0418774CB3D7599B4".toLowerCase()]: {
+      fee: "3000",
+      pairId: "5",
+    },
+    ["0xDA10009cBd5D07dd0CeCc66161FC93D7c9000da1".toLowerCase()]: {
+      fee: "3000",
+      pairId: "6",
+    },
   },
+  "0x4200000000000000000000000000000000000042": {
+    ["0x7F5c764cBc14f9669B88837ca1490cCa17c31607".toLowerCase()]: {
+      fee: "3000",
+      pairId: "2",
+    },
+  },
+  ["0x7F5c764cBc14f9669B88837ca1490cCa17c31607".toLowerCase()]: {
+    ["0xDA10009cBd5D07dd0CeCc66161FC93D7c9000da1".toLowerCase()]: {
+      fee: "100",
+      pairId: "4",
+    },
+  }
 }
 
 function normalize(token:string, weth:string){
@@ -61,8 +86,7 @@ return {} // We don't support swaps to WETH
   )
 
   const inputIsETH = from === ethers.constants.AddressZero;
-  const slippage = BigNumber.from((100-Number(extra.slippage))*1e4)
-  const calldata = encode(pair.pairId, token0isTokenIn, quotedAmountOut.mul(slippage).div(1e6), inputIsETH, false, amount)
+  const calldata = encode(pair.pairId, token0isTokenIn, quotedAmountOut, extra.slippage ?? '0.5', inputIsETH, false, amount)
 if(calldata.length > (256/4 + 2)){
 return {} // LlamaZip doesn't support calldata that's bigger than one EVM word
 }
