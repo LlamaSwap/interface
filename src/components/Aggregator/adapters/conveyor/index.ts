2507666cec949dd40f8abc478dc17bed7a02a066
0xOsiris <djosiris@proton.me>
2023-03-06 13:29:41 -0600

    Added chainToId api remmaping

---
+++ src/components/Aggregator/adapters/conveyor/index.ts
@@ -0,0 +1,9 @@
+export const chainToId = {
+	ethereum: 'https://ethereum.conveyor.finance/',
+	bsc: 'https://bsc.conveyor.finance/',
+	polygon: 'https://polygon.conveyor.finance/',
+	optimism: 'https://optimism.conveyor.finance/',
+	arbitrum: 'https://arbitrum.conveyor.finance/',
+	avax: 'https://avalanche.conveyor.finance/',
+	fantom: 'https://fantom.conveyor.finance/',
+};
