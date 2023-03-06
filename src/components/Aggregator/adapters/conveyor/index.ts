1e4fa483ec93ce033ef2a5ced48cdcf48d0714d9
0xOsiris <djosiris@proton.me>
2023-03-06 13:32:44 -0600

    Added conveyorSwapAggregatorAddress chainId remmaping

--- src/components/Aggregator/adapters/conveyor/index.ts
+++ src/components/Aggregator/adapters/conveyor/index.ts
@@ -7,3 +7,13 @@ export const chainToId = {
 	avax: 'https://avalanche.conveyor.finance/',
 	fantom: 'https://fantom.conveyor.finance/',
 };
+
+const conveyorSwapAggregatorAddress = {
+    1: '0x9536e7Ad58006610eCE944B98a8768d9480a6176',
+    56: '0xCd1BA99aF51CcFcffdEa7F466D6A8D5AF81c5e6E',
+    137: '0x001DC0C386E020b526a17C30c1ACB93f49B1236e',
+    10: '0x6F7413E93F4221048eB51C10645C7377d5750eb3',
+    42161: '0xD9230DFA9ee25E007173C4b409F19B516b83066d',
+    43114: '0xb31f66aa3c1e785363f0875a1b74e27b85fd66c7',
+    250: '0x21be370d5312f44cb42ce377bc9b8a0cef1a4c83',
+}
