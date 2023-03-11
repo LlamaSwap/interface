61b0796e2deac7d73fe5cf3e9f9f5425ae7004b6
vrotend <vrotend@proton.me>
2023-03-12 01:19:58 +0300

    Fb trace (#129)
    * fix tx data for firebird traces
    
    * fix

--- src/components/Aggregator/adapters/firebird.ts
+++ src/components/Aggregator/adapters/firebird.ts
@@ -110,4 +110,5 @@ export async function swap({ signer, rawQuote, chain }) {
 
 export const getTxData = ({ rawQuote }) => rawQuote?.tx?.data;
 
-export const getTx = ({ rawQuote }) => rawQuote?.tx;
+
+export const getTx = ({ rawQuote }) => ({ ...rawQuote?.tx, to: rawQuote?.tx?.router });
