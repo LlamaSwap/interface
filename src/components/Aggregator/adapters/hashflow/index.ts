24ee56e634072023c916eec908934adbf167f7d4
vrtnd <vrotend@proton.me>
2023-03-13 19:22:54 +0300

    remove unused hashflow param

--- src/components/Aggregator/adapters/hashflow/index.ts
+++ src/components/Aggregator/adapters/hashflow/index.ts
@@ -29,8 +29,7 @@ const routerAddress = {
 };
 
 export async function getQuote(chain: string, from: string, to: string, amount: string, extra) {
-	const amountParam =
-		extra.amountOut && extra.amountOut !== '0' ? { quoteTokenAmount: extra.amountOut } : { baseTokenAmount: amount };
+	const amountParam = { baseTokenAmount: amount };
 
 	const data = await fetch(`https://api.hashflow.com/taker/v2/rfq`, {
 		method: 'POST',
