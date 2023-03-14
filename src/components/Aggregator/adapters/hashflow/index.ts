d2a34a41d6ebce950b41470ddf02033d353bfe49
vrtnd <vrotend@proton.me>
2023-03-14 16:03:03 +0300

    Revert "remove unused hashflow param"
    This reverts commit 24ee56e634072023c916eec908934adbf167f7d4.

--- src/components/Aggregator/adapters/hashflow/index.ts
+++ src/components/Aggregator/adapters/hashflow/index.ts
@@ -29,7 +29,8 @@ const routerAddress = {
 };
 
 export async function getQuote(chain: string, from: string, to: string, amount: string, extra) {
-	const amountParam = { baseTokenAmount: amount };
+	const amountParam =
+		extra.amountOut && extra.amountOut !== '0' ? { quoteTokenAmount: extra.amountOut } : { baseTokenAmount: amount };
 
 	const data = await fetch(`https://api.hashflow.com/taker/v2/rfq`, {
 		method: 'POST',
