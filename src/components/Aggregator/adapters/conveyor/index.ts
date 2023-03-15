fa083c5158373301ba7c9b0548bf310421d511e6
0xOsiris <djosiris@proton.me>
2023-03-15 14:00:40 -0500

    Gas estimation

--- src/components/Aggregator/adapters/conveyor/index.ts
+++ src/components/Aggregator/adapters/conveyor/index.ts
@@ -1,81 +1,85 @@
 import { BigNumber, ethers } from 'ethers';
 
+import { chainsMap } from '../../constants';
 import { sendTx } from '../../utils/sendTx';
 
-const chainToChainId = {
-	ethereum: 1,
-	bsc: 56,
-	polygon: 137,
-	optimism: 10,
-	arbitrum: 42161,
-	avax: 43114,
-	fantom: 250
-};
+const baseUrl = "https://api.conveyor.finance/"
+
+export const native = '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE' //Burn address represents native token in the api
 
 export const chainToId = {
-	ethereum: 'https://ethereum.conveyor.finance/',
-	bsc: 'https://bsc.conveyor.finance/',
-	polygon: 'http://127.0.0.1:8080/',
-	optimism: 'https://optimism.conveyor.finance/',
-	arbitrum: 'https://arbitrum.conveyor.finance/',
-	avax: 'https://avalanche.conveyor.finance/',
-	fantom: 'https://fantom.conveyor.finance/'
+	ethereum: `${baseUrl}ethereum/`,
+	bsc: `${baseUrl}bsc/`,
+	polygon: `${baseUrl}polygon/`,
+	optimism: `${baseUrl}optimism/`,
+	arbitrum: `${baseUrl}arbitrum/`,
+	avax: `${baseUrl}avalanche/`,
+	fantom: `${baseUrl}fantom/`
 };
 
 const conveyorSwapAggregatorAddress = {
-	1: '0x9536e7Ad58006610eCE944B98a8768d9480a6176',
-	56: '0xCd1BA99aF51CcFcffdEa7F466D6A8D5AF81c5e6E',
-	137: '0x001DC0C386E020b526a17C30c1ACB93f49B1236e',
-	10: '0x6F7413E93F4221048eB51C10645C7377d5750eb3',
-	42161: '0xD9230DFA9ee25E007173C4b409F19B516b83066d',
-	43114: '0xb31f66aa3c1e785363f0875a1b74e27b85fd66c7',
-	250: '0x21be370d5312f44cb42ce377bc9b8a0cef1a4c83'
+	1: '0x7B68636A43c9aC79fA0ef423d5094b291ffEFAb9',
+	56: '0xD9230DFA9ee25E007173C4b409F19B516b83066d',
+	137: '0x62eC8d2d797216b2f7784646d0646fe923461806',
+	10: '0x91AE75251Bc0c6654EF0B327D190877B49b21A2E',
+	42161: '0x85D6592B20a00551493B5264c3d42C0378c9800b',
+	43114: '0xBCbCF359E55EfA0bB9422C4a6aeCF7Ef7998898C',
+	250: '0x5C5482520387E7B9875965fA1dA7888424b6c2E7'
 };
 
 export const name = 'Conveyor';
 
 export async function getQuote(chain: string, from: string, to: string, amount: string, extra) {
-	console.log('Conveyor', chain, from, to, amount, extra);
-	const data = await fetch(chainToId[chain], {
+	const chainId = chainsMap[chain];
+
+	const data = await fetch(chainToId[chain],{ 
 		method: 'POST',
 		body: JSON.stringify({
-			token_in: from,
-			token_out: to,
+			token_in: isNativeToken(from) ? native : from,
+			token_out: isNativeToken(to) ? native : to,
 			amount_in: BigNumber.from(amount).toHexString(),
-			chain_id: chainToChainId[chain],
+			chain_id: chainId,
 			from_address: extra.userAddress ?? ethers.constants.AddressZero, //Pass the zero address if no address is connected to get a quote back from the saapi
-			allowed_slippage: Number(extra.slippage) * 100
+			allowed_slippage: Number(extra.slippage) * 100 //Saapi expects slippage in Bips
 		}),
 
 		headers: {
 			'Content-Type': 'application/json'
 		}
 	}).then((r) => r.json());
-	console.log('Conveyor data', data);
-	console.log(from, to)
+
+	let gas = !isZero(data.gas_estimate) ? data.gas_estimate : 0; //The api returns a 1.25% margin on the gas estimate from the provider
+	
 	return {
 		amountReturned: data.amount_out,
-		estimateGas: data.gas_estimate,
-		tokenApprovalAddress: conveyorSwapAggregatorAddress[chainToChainId[chain]],
+		amountIn: amount,
+		estimatedGas: gas,
+		tokenApprovalAddress: conveyorSwapAggregatorAddress[chainId],
 		rawQuote: {
 			...data,
-			from: extra.userAddress,
-			to: conveyorSwapAggregatorAddress[chainToChainId[chain]],
-			tx: { data: data?.tx_calldata, gasLimit: '300000' }
+			gasLimit: BigNumber.from(gas).toString(),
+			tx: {data: data?.tx_calldata, ...(isNativeToken(from) ? {value: amount} :{}) }
 		}
 	};
 }
 
+function isZero(value: string): boolean {
+	return BigNumber.from(value).isZero();
+}
+
+function isNativeToken(token: string): boolean {
+	return token === ethers.constants.AddressZero;
+}
+
 export async function swap({ signer, rawQuote, chain }) {
 	const tx = await sendTx(signer, chain, {
-		from:rawQuote.from,
-		to: rawQuote.to,
 		...rawQuote.tx
 	});
 
 	return tx;
 }
 
-export const getTxData = ({ rawQuote }) => rawQuote?.tx_calldata;
+export const getTxData = ({ rawQuote }) => rawQuote?.tx?.data;
 
 
+export const getTx = ({ rawQuote }) => rawQuote.tx;
