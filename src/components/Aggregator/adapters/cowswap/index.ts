aa3a1bb7fadb24e8978a58d9b178655edbf0e9a2
vrtnd <vrotend@proton.me>
2023-03-16 02:30:11 +0300

    fix

--- src/components/Aggregator/adapters/cowswap/index.ts
+++ src/components/Aggregator/adapters/cowswap/index.ts
@@ -90,7 +90,7 @@ export async function getQuote(chain: string, from: string, to: string, amount: 
 
 	return {
 		amountReturned: expectedBuyAmount,
-		amountIn: BigNumber(data?.quote.sellAmount).plus(data.quote.feeAmount).toString() || '0',
+		amountIn: BigNumber(data?.quote.sellAmount).plus(data.quote.feeAmount).toFixed(0) || '0',
 		estimatedGas: isEthflowOrder ? 56360 : 0, // 56360 is gas from sending createOrder() tx
 		validTo: data.quote?.validTo || 0,
 		rawQuote: { ...data, slippage: extra.slippage },
