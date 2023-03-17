5fcc48c994c51a4e2dd11115513c416f45dddc41
0xngmi <80541789+0xngmi@users.noreply.github.com>
2023-03-17 19:01:13 +0000

    add arbitrum to llamazip (#142)
    * add arbitrum to llamazip
    
    * fix
    
    * merge cleanup
    
    * deduplicate routers and fix fee
    
    * remove default slippage
    
    ---------
    
    Co-authored-by: vrtnd <vrotend@proton.me>

--- src/components/Aggregator/adapters/llamazip/index.ts
+++ src/components/Aggregator/adapters/llamazip/index.ts
@@ -1,118 +1,96 @@
-import { BigNumber, ethers } from "ethers";
-import { providers } from "../../rpcs";
-import { sendTx } from "../../utils/sendTx";
-import { encode } from "./encode";
+import { BigNumber, ethers } from 'ethers';
+import { providers } from '../../rpcs';
+import { sendTx } from '../../utils/sendTx';
+import { encode } from './encode';
+import { normalizeTokens, pairs } from './pairs';
 
 export const name = 'LlamaZip';
 export const token = 'none';
 
 export const chainToId = {
-  optimism: '0x6f9d14Cf4A06Dd9C70766Bd161cf8d4387683E1b',
+	optimism: '0x6f9d14Cf4A06Dd9C70766Bd161cf8d4387683E1b',
+	arbitrum: '0x5279EBC4e5BA9eA09F19ADE49F2Bc98339aeA4d7'
 };
 
 // https://docs.uniswap.org/contracts/v3/reference/deployments
 const quoter = {
-  optimism: "0xb27308f9F90D607463bb33eA1BeBb41C27CE5AB6"
-}
+	optimism: '0xb27308f9F90D607463bb33eA1BeBb41C27CE5AB6',
+	arbitrum: '0xb27308f9F90D607463bb33eA1BeBb41C27CE5AB6'
+};
 
 const weth = {
-  optimism: "0x4200000000000000000000000000000000000006"
-}
+	optimism: '0x4200000000000000000000000000000000000006',
+	arbitrum: '0x82aF49447D8a07e3bd95BD0d56f35241523fBab1'
+};
 
-// Smaller token first, addresses always in lowercase
-const pairs = {
-  "0x4200000000000000000000000000000000000006": {
-    ["0x7F5c764cBc14f9669B88837ca1490cCa17c31607".toLowerCase()]: {
-      fee: "500",
-      pairId: "0",
-    },
-    ["0x4200000000000000000000000000000000000042".toLowerCase()]: {
-      fee: "3000",
-      pairId: "1",
-    },
-    // pool 3 is ignored because we already have one with same tokens
-    ["0x8700dAec35aF8Ff88c16BdF0418774CB3D7599B4".toLowerCase()]: {
-      fee: "3000",
-      pairId: "5",
-    },
-    ["0xDA10009cBd5D07dd0CeCc66161FC93D7c9000da1".toLowerCase()]: {
-      fee: "3000",
-      pairId: "6",
-    },
-  },
-  "0x4200000000000000000000000000000000000042": {
-    ["0x7F5c764cBc14f9669B88837ca1490cCa17c31607".toLowerCase()]: {
-      fee: "3000",
-      pairId: "2",
-    },
-  },
-  ["0x7F5c764cBc14f9669B88837ca1490cCa17c31607".toLowerCase()]: {
-    ["0xDA10009cBd5D07dd0CeCc66161FC93D7c9000da1".toLowerCase()]: {
-      fee: "100",
-      pairId: "4",
-    },
-  }
-}
-
-function normalize(token:string, weth:string){
-  return (token === ethers.constants.AddressZero ? weth : token).toLowerCase()
+function normalize(token: string, weth: string) {
+	return (token === ethers.constants.AddressZero ? weth : token).toLowerCase();
 }
 
 // https://docs.uniswap.org/sdk/v3/guides/quoting
 export async function getQuote(chain: string, from: string, to: string, amount: string, extra: any) {
-  if (to.toLowerCase() === weth[chain].toLowerCase()) {
-    return {} // We don't support swaps to WETH
-  }
-  const provider = providers[chain];
-  const quoterContract = new ethers.Contract(quoter[chain], 
-    ["function quoteExactInputSingle(address tokenIn,address tokenOut,uint24 fee,uint256 amountIn,uint160 sqrtPriceLimitX96) external returns (uint256 amountOut)"],
-    provider);
+	if (to.toLowerCase() === weth[chain].toLowerCase()) {
+		return {}; // We don't support swaps to WETH
+	}
+	const provider = providers[chain];
+	const quoterContract = new ethers.Contract(
+		quoter[chain],
+		[
+			'function quoteExactInputSingle(address tokenIn,address tokenOut,uint24 fee,uint256 amountIn,uint160 sqrtPriceLimitX96) external returns (uint256 amountOut)'
+		],
+		provider
+	);
 
-  const tokenFrom = normalize(from, weth[chain]);
-  const tokenTo = normalize(to, weth[chain]);
+	const tokenFrom = normalize(from, weth[chain]);
+	const tokenTo = normalize(to, weth[chain]);
 
-  const token0isTokenIn = BigNumber.from(tokenFrom).lt(tokenTo)
-  const pair = pairs[token0isTokenIn?tokenFrom:tokenTo]?.[token0isTokenIn?tokenTo:tokenFrom]
-  if(pair === undefined){
-    return {}
-  }
+	const token0isTokenIn = BigNumber.from(tokenFrom).lt(tokenTo);
 
-  const quotedAmountOut = await quoterContract.callStatic.quoteExactInputSingle(
-    tokenFrom,
-    tokenTo,
-    pair.fee,
-    amount,
-    0
-  )
+	const pair = pairs[chain as keyof typeof pairs].find(
+		({ name }) => name === normalizeTokens(tokenFrom, tokenTo).join('-')
+	);
 
-  const inputIsETH = from === ethers.constants.AddressZero;
-  const calldata = encode(pair.pairId, token0isTokenIn, quotedAmountOut, extra.slippage, inputIsETH, false, amount)
-  if (calldata.length > (256 / 4 + 2)) {
-    return {} // LlamaZip doesn't support calldata that's bigger than one EVM word
-  }
+	if (pair === undefined) {
+		return {};
+	}
 
-  return {
-    amountReturned: quotedAmountOut.toString(),
-    estimatedGas: 200e3.toString(), // random approximation
-    rawQuote: {
-      tx: {
-        to: chainToId[chain],
-        data: calldata,
-        ...(inputIsETH ? { value: amount } : {}),
-      }
-    },
-    tokenApprovalAddress: chainToId[chain],
-    logo: 'https://raw.githubusercontent.com/DefiLlama/memes/master/bussin.jpg'
-  };
+	const quotedAmountOut = await quoterContract.callStatic.quoteExactInputSingle(
+		tokenFrom,
+		tokenTo,
+		pair.fee,
+		amount,
+		0
+	);
+
+	const inputIsETH = from === ethers.constants.AddressZero;
+	const calldata = encode(pair.pairId, token0isTokenIn, quotedAmountOut, extra.slippage, inputIsETH, false, amount);
+	if (calldata.length > 256 / 4 + 2) {
+		return {}; // LlamaZip doesn't support calldata that's bigger than one EVM word
+	}
+
+	return {
+		amountReturned: quotedAmountOut.toString(),
+		estimatedGas: (200e3).toString(), // random approximation
+		rawQuote: {
+			tx: {
+				to: chainToId[chain],
+				data: calldata,
+				...(inputIsETH ? { value: amount } : {})
+			}
+		},
+		tokenApprovalAddress: chainToId[chain],
+		logo: 'https://raw.githubusercontent.com/DefiLlama/memes/master/bussin.jpg'
+	};
 }
 
 export async function swap({ signer, rawQuote, chain }) {
-  const fromAddress = await signer.getAddress();
+	const fromAddress = await signer.getAddress();
+
 	const tx = await sendTx(signer, chain, {
 		from: fromAddress,
 		to: rawQuote.tx.to,
 		data: rawQuote.tx.data,
-		value: rawQuote.tx.value,
+		value: rawQuote.tx.value
 	});
 	return tx;
 }
