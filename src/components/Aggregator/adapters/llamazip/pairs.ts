bc0b61faefd7015245bdb62536ce834dc9c179d1
0xngmi <0xngmi@protonmail.com>
2023-03-21 19:22:37 +0000

    fix token comparison

--- src/components/Aggregator/adapters/llamazip/pairs.ts
+++ src/components/Aggregator/adapters/llamazip/pairs.ts
@@ -1,3 +1,5 @@
+import BigNumber from 'bignumber.js';
+
 export const tokens = {
 	optimism: {
 		weth: '0x4200000000000000000000000000000000000006',
@@ -25,7 +27,7 @@ export const tokens = {
 };
 
 export const normalizeTokens = (t0, t1) =>
-	Number(t0) < Number(t1) ? [t0.toLowerCase(), t1.toLowerCase()] : [t1.toLowerCase(), t0.toLowerCase()];
+	BigNumber(t0).lt(t1) ? [t0.toLowerCase(), t1.toLowerCase()] : [t1.toLowerCase(), t0.toLowerCase()];
 
 const createPair = (t0: string, t1: string, fee: string, pairId: string) => {
 	const [token0, token1] = normalizeTokens(t0, t1);
