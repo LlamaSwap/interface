9025cf297b847330d4e07856051901616820c083
Justin Deible <justin@semiotic.ai>
2023-04-06 14:13:46 -0700

    update formatting and request params

--- src/components/Aggregator/adapters/odos.ts
+++ src/components/Aggregator/adapters/odos.ts
@@ -6,17 +6,17 @@ import { sendTx } from '../utils/sendTx';
 
 export const chainToId = {
 	ethereum: 1,
-  optimism: 10,
+	optimism: 10,
 	bsc: 56,
 	polygon: 137,
-  arbitrum: 42161,
+	arbitrum: 42161,
 	avax: 43114,
 };
 
 const approvalAddresses = {
 	ethereum: '0x76f4eeD9fE41262669D0250b2A97db79712aD855',
-  optimism: '0x69Dd38645f7457be13571a847FfD905f9acbaF6d',
-	bsc: '0x69Dd38645f7457be13571a847FfD905f9acbaF6d',
+	optimism: '0x69Dd38645f7457be13571a847FfD905f9acbaF6d',
+	bsc: '0x9f138be5aA5cC442Ea7cC7D18cD9E30593ED90b9',
 	polygon: '0xa32EE1C40594249eb3183c10792BcF573D4Da47C',
 	arbitrum: '0xdd94018F54e565dbfc939F7C44a16e163FaAb331',
 	avax: '0xfE7Ce93ac0F78826CD81D506B07Fe9f459c00214',
@@ -35,52 +35,52 @@ export async function getQuote(
 	from: string,
 	to: string,
 	amount: string,
-	{ fromToken, toToken, userAddress, slippage, amountOut }
+	{ userAddress, slippage }
 ) {
 	// ethereum = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE
 	// amount should include decimals
 
-  const tokenFrom = from === ethers.constants.AddressZero ? nativeToken : from;
+	const tokenFrom = from === ethers.constants.AddressZero ? nativeToken : from;
 	const tokenTo = to === ethers.constants.AddressZero ? nativeToken : to;
 
-  const quoteData = await fetch(`https://api.odos.xyz/sor/quote`, {
-      method: 'POST',
-      body: JSON.stringify({
-        chainId: chainToId[chain],
-        inputTokens: [
-          {
-            tokenAddress: tokenFrom,
-            amount: amount
-          }
-        ],
-        outputTokens: [
-          {
-            tokenAddress: tokenTo,
-            proportion: 1
-          }
-        ],
-        gasPrice: 100,
-        userAddr: userAddress !== ethers.constants.AddressZero ? userAddress : null,
-        slippageLimitPercent: slippage * 100,
-      }),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    }).then((r) => r.json());
+	const quoteData = await fetch(`https://api.odos.xyz/sor/quote`, {
+			method: 'POST',
+			body: JSON.stringify({
+				chainId: chainToId[chain],
+				inputTokens: [
+					{
+						tokenAddress: tokenFrom,
+						amount: amount
+					}
+				],
+				outputTokens: [
+					{
+						tokenAddress: tokenTo,
+						proportion: 1
+					}
+				],
+				userAddr: userAddress !== ethers.constants.AddressZero ? userAddress : null,
+				slippageLimitPercent: slippage,
+				disableRFQs: true,
+			}),
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		}).then((r) => r.json());
 
-  const swapData =
+	const swapData =
 		userAddress !== ethers.constants.AddressZero
 			? await fetch(`https://api.odos.xyz/sor/assemble`, {
 					method: 'POST',
 					body: JSON.stringify({
-            userAddr: userAddress,
-            pathId: quoteData.pathId,
-            simulate: false
-          }),
+						userAddr: userAddress,
+						pathId: quoteData.pathId,
+						simulate: false
+					}),
 					headers: {
 						'Content-Type': 'application/json'
 					}
-			  }).then((r) => r.json())
+				}).then((r) => r.json())
 			: null;
 
 	return {
@@ -108,13 +108,13 @@ export async function swap({ signer, rawQuote, chain }) {
 export const getTxData = ({ rawQuote }) => rawQuote?.data;
 
 export const getTx = ({ rawQuote }) => {
-  if (rawQuote === null) {
+	if (rawQuote === null) {
 		return {};
 	}
-  return {
-  	from: rawQuote.from,
-  	to: rawQuote.to,
-  	data: rawQuote.data,
-  	value: rawQuote.value
-  }
+	return {
+		from: rawQuote.from,
+		to: rawQuote.to,
+		data: rawQuote.data,
+		value: rawQuote.value
+	}
 };
