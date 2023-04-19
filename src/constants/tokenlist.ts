045f82d3d41d57f65c41332dad860173667a311a
vrtnd <vrotend@proton.me>
2023-04-19 21:32:30 +0300

    Old rdnt logo

--- src/constants/tokenlist.ts
+++ src/constants/tokenlist.ts
@@ -7,5 +7,16 @@ export const ownTokenList = [
 			'https://raw.githubusercontent.com/sushiswap/list/master/logos/token-logos/network/arbitrum/0x0C4681e6C0235179ec3D4F4fc4DF3d14FDD96017.jpg',
 		name: 'Radiant v2',
 		symbol: 'RDNT'
+	},
+	{
+		address: '0x0c4681e6c0235179ec3d4f4fc4df3d14fdd96017',
+		chainId: 42161,
+		decimals: 18,
+		logoURI:
+			'https://raw.githubusercontent.com/traderjoe-xyz/joe-tokenlists/main/logos/0x0C4681e6C0235179ec3D4F4fc4DF3d14FDD96017/logo.png',
+		ownLogoURI:
+			'https://raw.githubusercontent.com/traderjoe-xyz/joe-tokenlists/main/logos/0x0C4681e6C0235179ec3D4F4fc4DF3d14FDD96017/logo.png',
+		name: 'Radiant OLD',
+		symbol: 'RDNT'
 	}
 ];
