9d8eae6a643caff6801d2d8fb17c921034d1e4c2
strobie <107649472+0xStrobe@users.noreply.github.com>
2023-05-19 18:07:42 +0200

    icons improvements (#195)
    * use 48x48 icons
    
    * set new token icons endpoint

--- src/components/Aggregator/index.tsx
+++ src/components/Aggregator/index.tsx
@@ -336,7 +336,7 @@ export function AggregatorContainer({ tokenList, sandwichList }) {
 						address: fromToken2.address,
 						value: fromToken2.address,
 						decimals: fromToken2.decimals,
-						logoURI: `https://icons.llamao.fi/icons/tokens/${selectedChain.id || 1}/${fromToken2.address}?h=20&w=20`,
+						logoURI: `https://token-icons.llamao.fi/icons/tokens/${selectedChain.id || 1}/${fromToken2.address}?h=20&w=20`,
 						chainId: selectedChain.id || 1,
 						geckoId: null
 				  }
@@ -351,7 +351,7 @@ export function AggregatorContainer({ tokenList, sandwichList }) {
 						address: toToken2.address,
 						value: toToken2.address,
 						decimals: toToken2.decimals,
-						logoURI: `https://icons.llamao.fi/icons/tokens/${selectedChain.id || 1}/${toToken2.address}?h=20&w=20`,
+						logoURI: `https://token-icons.llamao.fi/icons/tokens/${selectedChain.id || 1}/${toToken2.address}?h=20&w=20`,
 						chainId: selectedChain.id || 1,
 						geckoId: null
 				  }
