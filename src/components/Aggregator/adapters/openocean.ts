c9267de34438d69debf4cdcb63bd1f6b0351dcea
0xngmi <0xngmi@protonmail.com>
2023-09-01 20:30:15 +0100

    delist openocean from all new chains

--- src/components/Aggregator/adapters/openocean.ts
+++ src/components/Aggregator/adapters/openocean.ts
@@ -17,13 +17,13 @@ export const chainToId = {
 	boba: 288,
 	okexchain: 66,
 	cronos: 25,
-	moonriver: 1285,
+	moonriver: 1285
 	//ontology: 58,
 	//polygonzkevm: 1101,
-	kava: 2222,
-	metis: 1088,
-	zksync: 324,
-	linea: 59144
+	//kava: 2222,
+	//metis: 1088,
+	//zksync: 324,
+	//linea: 59144
 };
 
 export const name = 'OpenOcean';
