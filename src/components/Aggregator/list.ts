7939ebe743982f40aec9891fe48b264370ee05e8
0xngmi <0xngmi@protonmail.com>
2024-01-16 00:17:03 +0000

    re-enable kyberswap

--- src/components/Aggregator/list.ts
+++ src/components/Aggregator/list.ts
@@ -2,7 +2,7 @@ import * as matcha from './adapters/0x';
 import * as inch from './adapters/1inch';
 import * as cowswap from './adapters/cowswap';
 import * as firebird from './adapters/firebird';
-//import * as kyberswap from './adapters/kyberswap';
+import * as kyberswap from './adapters/kyberswap';
 import * as hashflow from './adapters/hashflow';
 import * as openocean from './adapters/openocean';
 import * as paraswap from './adapters/paraswap';
@@ -16,7 +16,7 @@ import * as yieldyak from './adapters/yieldyak';
 import * as llamazip from './adapters/llamazip';
 // import * as krystal from './adapters/krystal'
 
-export const adapters = [matcha, inch, cowswap, openocean, yieldyak, paraswap, firebird, hashflow, llamazip];
+export const adapters = [matcha, inch, cowswap, openocean, yieldyak, paraswap, firebird, hashflow, llamazip, kyberswap];
 
 export const inifiniteApprovalAllowed = [matcha.name, inch.name, cowswap.name, paraswap.name];
 
