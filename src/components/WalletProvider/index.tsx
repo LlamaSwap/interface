16372720c4e88271645530bd5c8f52bdac389ff8
mintdart <96025197+mintdart@users.noreply.github.com>
2024-02-13 04:53:57 -0600

    update wallet list

--- src/components/WalletProvider/index.tsx
+++ src/components/WalletProvider/index.tsx
@@ -6,7 +6,27 @@ import { jsonRpcProvider } from 'wagmi/providers/jsonRpc';
 import styled from 'styled-components';
 import { allChains } from './chains';
 import { rpcsKeys } from '../Aggregator/rpcs';
-import { rabbyWallet, injectedWallet, walletConnectWallet, metaMaskWallet } from '@rainbow-me/rainbowkit/wallets';
+import {
+	rabbyWallet,
+	injectedWallet,
+	walletConnectWallet,
+	metaMaskWallet,
+	braveWallet,
+	coinbaseWallet,
+	phantomWallet,
+	argentWallet,
+	okxWallet,
+	bitskiWallet,
+	zerionWallet,
+	dawnWallet,
+	imTokenWallet,
+	mewWallet,
+	trustWallet,
+	xdefiWallet,
+	safeWallet,
+	omniWallet,
+	tahoWallet
+} from '@rainbow-me/rainbowkit/wallets';
 
 const { provider, chains } = configureChains(
 	[...allChains],
@@ -27,12 +47,33 @@ const Provider = styled.div`
 const projectId = 'b3d4ba9fb97949ab12267b470a6f31d2';
 const connectors = connectorsForWallets([
 	{
-		groupName: 'Recommended',
+		groupName: 'Popular',
 		wallets: [
 			injectedWallet({ chains }),
 			metaMaskWallet({ chains, projectId }),
 			walletConnectWallet({ projectId, chains }),
 			rabbyWallet({ chains })
+		]
+	},
+	{
+		groupName: 'Others',
+		wallets: [
+			braveWallet({ chains }),
+			coinbaseWallet({ chains, appName: 'LlamaSwap' }),
+			phantomWallet({ chains }),
+			argentWallet({ chains, projectId }),
+			safeWallet({ chains }),
+			okxWallet({ chains, projectId }),
+			bitskiWallet({ chains }),
+			zerionWallet({ chains, projectId }),
+			dawnWallet({ chains }),
+			imTokenWallet({ chains, projectId }),
+			mewWallet({ chains }),
+			trustWallet({ chains, projectId }),
+			xdefiWallet({ chains }),
+			dawnWallet({ chains }),
+			omniWallet({ chains, projectId }),
+			tahoWallet({ chains })
 		]
 	}
 ]);
