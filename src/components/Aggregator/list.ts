b5905c6a7417bc1c943d0523fb916bcd23ea0166
0xngmi <0xngmi@protonmail.com>
2024-03-18 02:09:36 +0000

    build odos integration

--- src/components/Aggregator/list.ts
+++ src/components/Aggregator/list.ts
@@ -3,7 +3,7 @@ import * as inch from './adapters/1inch';
 import * as cowswap from './adapters/cowswap';
 //import * as firebird from './adapters/firebird';
 import * as kyberswap from './adapters/kyberswap';
-import * as hashflow from './adapters/hashflow';
+//import * as hashflow from './adapters/hashflow';
 import * as openocean from './adapters/openocean';
 import * as paraswap from './adapters/paraswap';
 // import * as lifi from './adapters/lifi';
@@ -11,16 +11,16 @@ import * as paraswap from './adapters/paraswap';
 
 // import * as unidex from "./adapters/unidex" - disabled, their api is broken
 // import * as airswap from './adapters/airswap' cors
-// import * as odos from './adapters/odos';
+import * as odos from './adapters/odos';
 import * as yieldyak from './adapters/yieldyak';
 import * as llamazip from './adapters/llamazip';
 // import * as krystal from './adapters/krystal'
 
-export const adapters = [matcha, inch, cowswap, openocean, yieldyak, paraswap, hashflow, llamazip, kyberswap];
+export const adapters = [matcha, inch, cowswap, openocean, yieldyak, paraswap, llamazip, kyberswap, odos];
 
 export const inifiniteApprovalAllowed = [matcha.name, inch.name, cowswap.name, paraswap.name];
 
 export const adaptersWithApiKeys = {
-	[matcha.name]: true,
-	[hashflow.name]: true
+	[matcha.name]: true
+	//[hashflow.name]: true
 };
