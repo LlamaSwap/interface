baa1c321425623a6ccc2dbdb3e8a4adceb68e822
mintdart <96025197+mintdart@users.noreply.github.com>
2024-04-18 01:07:06 -0500

    add option to trade by sell amount

--- src/components/Aggregator/adapters/0xGasless.tsx
+++ src/components/Aggregator/adapters/0xGasless.tsx
@@ -17,6 +17,8 @@ const feeCollectorAddress = '0x9Ab6164976514F1178E2BB4219DA8700c9D96E9A';
 
 export const isGasless = true;
 
+export const isOutputAvailable = true;
+
 export async function getQuote(chain: string, from: string, to: string, amount: string, extra) {
 	// amount should include decimals
 
@@ -43,8 +45,8 @@ export async function getQuote(chain: string, from: string, to: string, amount: 
 	if (!data.liquidityAvailable) return null;
 
 	return {
-		amountReturned: data?.buyAmount || 0,
-		amountIn: data?.sellAmount || 0,
+		amountReturned: data?.buyAmount,
+		amountIn: data?.sellAmount,
 		rawQuote: data,
 		tokenApprovalAddress: data?.allowanceTarget ?? null,
 		logo: 'https://www.gitbook.com/cdn-cgi/image/width=40,height=40,fit=contain,dpr=2,format=auto/https%3A%2F%2F1690203644-files.gitbook.io%2F~%2Ffiles%2Fv0%2Fb%2Fgitbook-x-prod.appspot.com%2Fo%2Fspaces%252FKX9pG8rH3DbKDOvV7di7%252Ficon%252F1nKfBhLbPxd2KuXchHET%252F0x%2520logo.png%3Falt%3Dmedia%26token%3D25a85a3e-7f72-47ea-a8b2-e28c0d24074b'
