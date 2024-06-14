5566358d4427a648505b8043f3ef581db9748343
vrtnd <vrotend@proton.me>
2024-06-15 02:50:09 +0300

    Lending aggregator

--- src/components/Lending/index.tsx
+++ src/components/Lending/index.tsx
@@ -1,12 +1,14 @@
 import React, { useState, useRef, useMemo, useEffect } from 'react';
 import { useVirtualizer } from '@tanstack/react-virtual';
 import styled from 'styled-components';
-import { Box, Flex, Icon, Tooltip } from '@chakra-ui/react';
+import { Box, Flex, Icon, Tooltip, Input, Text, Button } from '@chakra-ui/react';
 import { ArrowRightIcon } from '@chakra-ui/icons';
 import ReactSelect from '../MultiSelect';
 import { ColumnHeader, RowContainer, YieldsBody, YieldsCell, YieldsContainer, YieldsWrapper } from '../Yields';
 import NotFound from './NotFound';
 import { formatAmountString } from '~/utils/formatAmount';
+import { useQuery } from '@tanstack/react-query';
+import { useRouter } from 'next/router';
 
 const ChainIcon = styled.img`
 	width: 24px;
@@ -15,54 +17,161 @@ const ChainIcon = styled.img`
 	border-radius: 50%;
 `;
 
-const YieldsRow = ({ data, index, style }) => (
-	<RowContainer style={style}>
-		<YieldsCell style={{ overflow: 'hidden', display: 'block', textAlign: 'center' }}>
-			{data[index].symbol} ➞ {data[index].borrowPool?.symbol}
-		</YieldsCell>
-		<YieldsCell>
-			<ChainIcon
-				src={`https://icons.llamao.fi/icons/protocols/${data[index].project}?w=48&h=48`}
-				alt={data[index].project}
-			/>
-		</YieldsCell>
-
-		<YieldsCell>
-			<Tooltip label={data[index].chain} aria-label={data[index].chain} placement="top">
-				<ChainIcon
-					src={`https://icons.llamao.fi/icons/chains/rsz_${data[index].chain.toLowerCase()}?w=48&h=48`}
-					alt={data[index].chain}
-				/>
-			</Tooltip>
-		</YieldsCell>
-		<YieldsCell
-			style={{
-				color: data[index].pairNetApy > 0 ? 'rgba(0, 255, 0, 0.6)' : 'rgba(255, 0, 0, 0.6)'
-			}}
-		>
-			{data[index].pairNetApy?.toFixed(2)}%
-		</YieldsCell>
-		<YieldsCell>{'$' + formatAmountString(data[index].borrowPool?.totalAvailableUsd)}</YieldsCell>
-	</RowContainer>
-);
+const YieldsRow = ({ data, index, style }) => {
+	const row = data[index];
+	const { lendUsdAmount, borrowUsdAmount, minAmountToLend } = row;
+	const lend =
+		minAmountToLend || lendUsdAmount
+			? formatAmountString(lendUsdAmount ? lendUsdAmount?.toFixed(2) : minAmountToLend?.toFixed(2), '$')
+			: '-';
+	const borrow = formatAmountString(borrowUsdAmount ? borrowUsdAmount?.toFixed(2) : '-', '$');
+	return (
+		<RowContainer style={style}>
+			<YieldsCell style={{ overflow: 'hidden', display: 'block', textAlign: 'center' }}>
+				{row.symbol} ➞ {row.borrowPool?.symbol}
+			</YieldsCell>
+			<YieldsCell>
+				<ChainIcon src={`https://icons.llamao.fi/icons/protocols/${row.project}?w=48&h=48`} alt={row.project} />
+			</YieldsCell>
+			<YieldsCell>
+				<Tooltip label={row.chain} aria-label={row.chain} placement="top">
+					<ChainIcon
+						src={`https://icons.llamao.fi/icons/chains/rsz_${row.chain.toLowerCase()}?w=48&h=48`}
+						alt={row.chain}
+					/>
+				</Tooltip>
+			</YieldsCell>
+			<YieldsCell
+				style={{
+					color: row.pairNetApy > 0 ? 'rgba(0, 255, 0, 0.6)' : 'rgba(255, 0, 0, 0.6)'
+				}}
+			>
+				{row.pairNetApy?.toFixed(2)}%
+			</YieldsCell>
+			<YieldsCell>{'$' + formatAmountString(row.borrowPool?.totalAvailableUsd)}</YieldsCell>
+			<YieldsCell>{lend}</YieldsCell>
+			<YieldsCell>{borrow}</YieldsCell>
+		</RowContainer>
+	);
+};
 
 const stablecoins = {
-	label: 'Stablecoins',
+	label: 'Stables',
 	value: 'STABLES',
 	logoURI: 'https://icons.llamao.fi/icons/pegged/usd_native?h=48&w=48'
 };
 
+const TokenAmountSelector = ({
+	tokenOptions,
+	selectedToken,
+	onTokenChange,
+	amount,
+	onAmountChange,
+	tokenPlaceholder,
+	amountPlaceholder,
+	isBorrow,
+	isDisabled
+}) => {
+	return (
+		<Box flex="3">
+			<Text fontSize={'14px'} pb="2" fontWeight={'bold'}>
+				{tokenPlaceholder}
+			</Text>
+			<ReactSelect
+				options={tokenOptions}
+				value={selectedToken}
+				onChange={onTokenChange}
+				placeholder={tokenPlaceholder}
+				isClearable
+				isDisabled={isDisabled}
+			/>
+			<Text fontSize={'14px'} pb="2" pt="2" fontWeight={'bold'}>
+				{amountPlaceholder}
+			</Text>
+			<Input
+				borderRadius={'12px'}
+				placeholder={selectedToken ? `${selectedToken?.label} amount` : 'Amount'}
+				value={amount}
+				onChange={isDisabled ? () => {} : onAmountChange}
+				bg="rgb(20, 22, 25)"
+				borderColor="transparent"
+				fontSize={'14px'}
+				_focusVisible={{ outline: 'none' }}
+			/>
+			{isBorrow ? (
+				<Flex gap="8px" mt="2" justify={'flex-end'}>
+					<Button onClick={() => onAmountChange({ target: { value: '25%' } })} size={'xs'}>
+						<Text fontSize={'14px'} fontWeight={'bold'}>
+							25%
+						</Text>
+					</Button>
+					<Button onClick={() => onAmountChange({ target: { value: '50%' } })} size={'xs'}>
+						<Text fontSize={'14px'} fontWeight={'bold'}>
+							50%
+						</Text>
+					</Button>
+					<Button onClick={() => onAmountChange({ target: { value: '75%' } })} size={'xs'}>
+						<Text fontSize={'14px'} fontWeight={'bold'}>
+							75%
+						</Text>
+					</Button>
+				</Flex>
+			) : null}
+		</Box>
+	);
+};
+
+const useGetPrices = (tokens) => {
+	const res = useQuery(
+		['getPrices', tokens],
+		async () => {
+			const prices = await fetch(`https://coins.llama.fi/prices/current/${tokens.join(',')}`).then((res) => res.json());
+			return prices;
+		},
+		{
+			enabled: tokens?.length > 0
+		}
+	);
+	return { ...res, data: res?.data?.coins };
+};
+
+const chainNameMap = {
+	avalanche: 'avax'
+};
+
+const mapChainName = (chain) => {
+	return chainNameMap[chain?.toLowerCase()] || chain;
+};
+
 const Lending = (props) => {
 	const { yields: initialData } = props;
+
+	const router = useRouter();
+
+	const { lendToken, borrowToken } = router.query;
 
 	const [lendingPools, setLendingPools] = useState([]);
 	const [borrowPools, setBorrowPools] = useState([]);
 	const [sortBy, setSortBy] = useState('');
 	const [sortDirection, setSortDirection] = useState('desc');
 	const containerRef = useRef(null);
-	const [selectedLendToken, setSelectedLendToken] = useState(null);
-	const [selectedBorrowToken, setSelectedBorrowToken] = useState(null);
+	const [selectedLendToken, setSelectedLendToken] = useState(lendToken);
+	const [selectedBorrowToken, setSelectedBorrowToken] = useState(borrowToken);
+	const [selectedChain, setSelectedChain] = useState(null);
+	const [amountToLend, setAmountToLend] = useState('');
+	const [amountToBorrow, setAmountToBorrow] = useState('');
 	const [poolPairs, setPoolPairs] = useState([]);
+	const { data: prices } = useGetPrices([
+		...new Set(
+			poolPairs
+				.map((p) =>
+					[p, p.borrowPool].map(
+						(p) => `${mapChainName(p?.chain?.toLowerCase())}:${p?.underlyingTokens?.[0]?.toLowerCase()}`
+					)
+				)
+				.flat()
+		)
+	]);
 
 	const rowVirtualizer = useVirtualizer({
 		count: poolPairs.length,
@@ -71,13 +180,6 @@ const Lending = (props) => {
 		overscan: 1000
 	});
 
-	const handleLendTokenChange = (selected) => {
-		setSelectedLendToken(selected);
-	};
-
-	const handleBorrowTokenChange = (selected) => {
-		setSelectedBorrowToken(selected);
-	};
 	const tokensList = useMemo(() => {
 		return [
 			stablecoins,
@@ -89,22 +191,39 @@ const Lending = (props) => {
 		];
 	}, [props.symbols]);
 
+	const chainList = useMemo(() => {
+		return initialData
+			.reduce((acc, pool) => {
+				if (!acc.includes(pool.chain)) {
+					acc.push(pool.chain);
+				}
+				return acc;
+			}, [])
+			.map((chain) => ({
+				label: chain,
+				value: chain,
+				logoURI: `https://icons.llamao.fi/icons/chains/rsz_${chain.toLowerCase()}?w=48&h=48`
+			}));
+	}, []);
 	useEffect(() => {
-		setLendingPools(
-			initialData.filter((p) => {
-				if (p?.stablecoin && selectedLendToken?.value === 'STABLES') return true;
-				return selectedLendToken ? p.symbol?.toLowerCase()?.includes(selectedLendToken?.value?.toLowerCase()) : false;
-			})
-		);
-		setBorrowPools(
-			initialData.filter((p) => {
-				if (p?.stablecoin && selectedBorrowToken?.value === 'STABLES') return true;
-				return selectedBorrowToken
-					? p.symbol?.toLowerCase()?.includes(selectedBorrowToken?.value?.toLowerCase())
+		const filterPools = (pools, token) => {
+			if (!token) return [];
+			return pools.filter((p) => {
+				const symbolMatch = p?.stablecoin
+					? token === 'STABLES'
+					: token
+					? p.symbol?.toLowerCase()?.includes(token?.toLowerCase())
 					: false;
-			})
-		);
-	}, [selectedLendToken, selectedBorrowToken]);
+
+				const chainMatch = selectedChain ? p.chain === selectedChain.value : false;
+
+				return symbolMatch && chainMatch;
+			});
+		};
+
+		setLendingPools(filterPools(initialData, selectedLendToken?.value));
+		setBorrowPools(filterPools(initialData, selectedBorrowToken?.value));
+	}, [selectedLendToken, selectedBorrowToken, selectedChain, initialData]);
 
 	useEffect(() => {
 		if (!selectedLendToken || !selectedBorrowToken) {
@@ -121,9 +240,35 @@ const Lending = (props) => {
 					return null;
 				}
 				const poolPairs = filteredBorrowPools.map((borrowPool) => {
-					const pairNetApy = lendPool.apy + borrowPool.apyBorrow * lendPool.ltv;
-					const pairRewardApy = lendPool.apyReward + borrowPool.apyRewardBorrow * lendPool.ltv;
-					return { ...lendPool, pairNetApy, pairRewardApy, borrowPool };
+					const lendTokenPrice =
+						prices?.[
+							`${mapChainName(lendPool?.chain?.toLowerCase())}:${lendPool?.underlyingTokens?.[0]?.toLowerCase()}`
+						]?.price;
+					const borrowTokenPrice =
+						prices?.[
+							`${mapChainName(borrowPool?.chain?.toLowerCase())}:${borrowPool?.underlyingTokens?.[0]?.toLowerCase()}`
+						]?.price;
+					const lendUsdAmount = lendTokenPrice * parseFloat(amountToLend);
+					const maxAmountToBorrow = lendUsdAmount * lendPool.ltv;
+					const borrowUsdAmount = amountToBorrow ? borrowTokenPrice * parseFloat(amountToBorrow) : maxAmountToBorrow;
+
+					const minAmountToLend = borrowUsdAmount / lendPool.ltv;
+					const lendBorrowRatio = lendUsdAmount / borrowUsdAmount || lendPool?.ltv;
+					const pairNetApy = lendPool.apy + borrowPool.apyBorrow * lendPool.ltv * lendBorrowRatio;
+					const pairRewardApy = lendPool.apyReward + borrowPool.apyRewardBorrow * lendPool.ltv * lendBorrowRatio;
+
+					return {
+						...lendPool,
+						pairNetApy,
+						pairRewardApy,
+						borrowPool,
+						lendUsdAmount,
+						borrowUsdAmount,
+						lendTokenPrice,
+						borrowTokenPrice,
+						maxAmountToBorrow,
+						minAmountToLend
+					};
 				});
 
 				return poolPairs;
@@ -132,8 +277,7 @@ const Lending = (props) => {
 			.flat();
 
 		setPoolPairs(pairs);
-	}, [lendingPools, borrowPools]);
-
+	}, [lendingPools, borrowPools, prices, selectedLendToken, selectedBorrowToken, amountToLend, amountToBorrow]);
 	const handleSort = (field) => {
 		setSortDirection((sortDirection) => (sortDirection === 'asc' ? 'desc' : 'asc'));
 		setSortBy(field);
@@ -147,70 +291,142 @@ const Lending = (props) => {
 		});
 	};
 
+	const resetFilters = () => {
+		setSelectedLendToken(null);
+		setSelectedBorrowToken(null);
+		setSelectedChain(null);
+		setAmountToLend('');
+		setAmountToBorrow('');
+	};
+
 	return (
-		<YieldsWrapper style={{ overflow: 'hidden', paddingBottom: '10px' }}>
-			<Flex mb={2} pr={4} pl={4} pt={4}>
-				<Box flex="3">
-					<ReactSelect
-						options={tokensList}
-						value={selectedLendToken}
-						onChange={handleLendTokenChange}
-						placeholder="Token to Lend"
-						isClearable
-					/>
-				</Box>
-				<Box mx={4} display={'flex'} flexDirection={'column'} justifyContent={'center'}>
-					<Flex alignItems="center" h="40px">
-						<Icon as={ArrowRightIcon} fontSize="24px" />
+		<div style={{ display: 'flex', gap: '16px' }}>
+			<YieldsWrapper style={{ overflow: 'hidden', paddingBottom: '10px', width: '600px' }}>
+				<Flex justifyContent={'center'} pt="4">
+					<Text fontSize={'20px'} fontWeight={'bold'}>
+						Pools
+					</Text>
+				</Flex>
+				{poolPairs.length === 0 ? (
+					<NotFound text={'Seclet a lending and borrowing token to see the available pairs.'} />
+				) : (
+					<YieldsContainer ref={containerRef}>
+						<ColumnHeader style={{ gridTemplateColumns: '1.4fr 1fr 1fr 1fr 1fr 1fr 1fr' }}>
+							<YieldsCell>Symbol</YieldsCell>
+							<YieldsCell>Project</YieldsCell>
+							<YieldsCell>Chain</YieldsCell>
+
+							<YieldsCell onClick={() => handleSort('pairNetApy')}>
+								Net APY {sortBy === 'pairNetApy' ? (sortDirection === 'asc' ? '↑' : '↓') : '↕'}
+							</YieldsCell>
+							<YieldsCell onClick={() => handleSort('tvlUsd')}>
+								Available {sortBy === 'tvlUsd' ? (sortDirection === 'asc' ? '↑' : '↓') : '↕'}
+							</YieldsCell>
+							<YieldsCell>Lend</YieldsCell>
+							<YieldsCell>Borrow</YieldsCell>
+						</ColumnHeader>
+						<YieldsBody style={{ height: `380px` }}>
+							{rowVirtualizer.getVirtualItems().map((virtualRow) => (
+								<YieldsRow
+									key={virtualRow.index}
+									data={poolPairs}
+									index={virtualRow.index}
+									style={{
+										gridTemplateColumns: '1.4fr 1fr 1fr 1fr 1fr 1fr 1fr',
+										position: 'absolute',
+										top: `${virtualRow.start}px`,
+										height: `${virtualRow.size}px`,
+										width: '100%'
+									}}
+								/>
+							))}
+						</YieldsBody>
+					</YieldsContainer>
+				)}
+			</YieldsWrapper>
+			<YieldsWrapper style={{ overflow: 'hidden', paddingBottom: '10px' }}>
+				<Flex justifyContent={'center'} pt="4">
+					<Text fontSize={'20px'} fontWeight={'bold'}>
+						Filters
+					</Text>
+				</Flex>
+				<Flex mb={2} pr={4} pl={4}>
+					<Flex pr={4} pl={4} pt={2} w="100%" flexDirection={'column'}>
+						<Text fontSize={'14px'} pb="2" fontWeight={'bold'}>
+							Chain
+						</Text>
+						<ReactSelect
+							value={selectedChain}
+							onChange={setSelectedChain}
+							options={chainList}
+							placeholder="Select chain..."
+							isClearable
+							style={{ width: '100%' }}
+						/>
 					</Flex>
-				</Box>
-				<Box flex="3">
-					<ReactSelect
-						options={tokensList}
-						value={selectedBorrowToken}
-						onChange={handleBorrowTokenChange}
-						placeholder="Token to Borrow"
-						isClearable
-					/>
-				</Box>
-			</Flex>
-			{poolPairs.length === 0 ? (
-				<NotFound
-					hasSelectedFilters={selectedBorrowToken && selectedLendToken}
-					defaultText="Seclet a lending and borrowing token to see the available pairs."
-				/>
-			) : (
-				<YieldsContainer ref={containerRef}>
-					<ColumnHeader style={{ gridTemplateColumns: '1.6fr 1fr 1fr 1fr 1fr' }}>
-						<YieldsCell>Symbol</YieldsCell>
-						<YieldsCell>Project</YieldsCell>
-						<YieldsCell>Chain</YieldsCell>
-						<YieldsCell onClick={() => handleSort('pairNetApy')}>
-							Net APY {sortBy === 'pairNetApy' ? (sortDirection === 'asc' ? '↑' : '↓') : '↕'}
-						</YieldsCell>
-						<YieldsCell onClick={() => handleSort('tvlUsd')}>
-							Available {sortBy === 'tvlUsd' ? (sortDirection === 'asc' ? '↑' : '↓') : '↕'}
-						</YieldsCell>
-					</ColumnHeader>
-					<YieldsBody style={{ height: `380px`, minHeight: '480px' }}>
-						{rowVirtualizer.getVirtualItems().map((virtualRow) => (
-							<YieldsRow
-								key={virtualRow.index}
-								data={poolPairs}
-								index={virtualRow.index}
-								style={{
-									gridTemplateColumns: '1.6fr 1fr 1fr 1fr 1fr',
-									position: 'absolute',
-									top: `${virtualRow.start}px`,
-									height: `${virtualRow.size}px`,
-									width: '100%'
-								}}
-							/>
-						))}
-					</YieldsBody>
-				</YieldsContainer>
-			)}
-		</YieldsWrapper>
+				</Flex>
+				<Flex mb={2} pr={4} pl={4}>
+					<Flex mb={2} pr={4} pl={4} pt={4}>
+						<TokenAmountSelector
+							tokenOptions={tokensList}
+							selectedToken={selectedLendToken}
+							onTokenChange={(token) => {
+								setSelectedLendToken(token),
+									router.push(
+										{
+											query: { ...router.query, lendToken: token?.value }
+										},
+										undefined,
+										{ shallow: true }
+									);
+							}}
+							amount={amountToLend}
+							onAmountChange={(e) => setAmountToLend(e.target.value)}
+							tokenPlaceholder="Token to Lend"
+							amountPlaceholder="Amount to Lend"
+						/>
+						<Box mx={4} display={'flex'} flexDirection={'column'} justifyContent={'center'}>
+							<Flex alignItems="center" h="40px">
+								<Icon as={ArrowRightIcon} fontSize="24px" />
+							</Flex>
+						</Box>
+						<TokenAmountSelector
+							tokenOptions={tokensList}
+							selectedToken={selectedBorrowToken}
+							onTokenChange={(token) => {
+								setSelectedBorrowToken(token),
+									router.push(
+										{
+											query: { ...router.query, lendToken: token?.value }
+										},
+										undefined,
+										{ shallow: true }
+									);
+							}}
+							amount={amountToBorrow}
+							onAmountChange={(e) => setAmountToBorrow(e.target.value)}
+							tokenPlaceholder="Token to Borrow"
+							amountPlaceholder="Amount to Borrow"
+							isBorrow
+						/>
+					</Flex>
+				</Flex>
+				<Button
+					onClick={resetFilters}
+					colorScheme="red"
+					mt="4"
+					size="sm"
+					variant="outline"
+					style={{
+						position: 'absolute',
+						bottom: '20px',
+						right: '20px'
+					}}
+				>
+					Clear Filters
+				</Button>
+			</YieldsWrapper>
+		</div>
 	);
 };
 
--- src/utils/formatAmount.ts
+++ src/utils/formatAmount.ts
@@ -1,11 +1,7 @@
 export const formatAmount = (amount: string | number) => amount.toString().trim().split(' ').join('');
 
-export const formatAmountString = (value) => {
-	const suffixes = ['', 'k', 'm', 'b', 't'];
-	const suffixNum = Math.floor(('' + value).length / 3);
-	let shortValue = parseFloat((suffixNum !== 0 ? value / Math.pow(1000, suffixNum) : value).toPrecision(3));
-	if (shortValue % 1 !== 0) {
-		shortValue = shortValue.toFixed(2) as any;
-	}
-	return shortValue + suffixes[suffixNum];
+export const formatAmountString = (value, prefix = '') => {
+	if (isNaN(value)) return value;
+	const formatter = Intl.NumberFormat('en', { notation: 'compact' });
+	return prefix + formatter.format(value);
 };
