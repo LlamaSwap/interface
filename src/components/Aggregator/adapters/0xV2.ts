22ca0c8605e467464924e57c28dc312a18ad3fac
mintdart <96025197+mintdart@users.noreply.github.com>
2024-06-14 07:58:02 -0500

    update query params

--- src/components/Aggregator/adapters/0xV2.ts
+++ src/components/Aggregator/adapters/0xV2.ts
@@ -40,7 +40,7 @@ export async function getQuote(chain: string, from: string, to: string, amount: 
 	const amountParam = `sellAmount=${amount}`;
 
 	const data = await fetch(
-		`https://api.0x.org/swap/permit2/quote?chainId=${chainToId[chain]}&buyToken=${tokenTo}&${amountParam}&sellToken=${tokenFrom}&slippageBps=${extra.slippage}&affiliateAddress=${defillamaReferrerAddress}&taker=${extra.userAddress}&tradeSurplusRecipient=${feeCollectorAddress}`,
+		`https://api.0x.org/swap/permit2/quote?chainId=${chainToId[chain]}&buyToken=${tokenTo}&${amountParam}&sellToken=${tokenFrom}&swapFeeBps=0&slippageBps=${extra.slippage}&affiliateAddress=${defillamaReferrerAddress}&taker=${extra.userAddress}&tradeSurplusRecipient=${feeCollectorAddress}`,
 		{
 			headers: {
 				'0x-api-key': process.env.OX_API_KEY
