c27914eaf1509c79e7d06061aa968ae0cfc02f2b
0xngmi <0xngmi@protonmail.com>
2024-12-25 13:16:38 +0000

    change list

--- src/components/Aggregator/list.ts
+++ src/components/Aggregator/list.ts
@@ -1,5 +1,5 @@
 import * as matcha from './adapters/0x';
-//import * as inch from './adapters/1inch';
+import * as inch from './adapters/1inch';
 import * as cowswap from './adapters/cowswap';
 //import * as firebird from './adapters/firebird';
 import * as kyberswap from './adapters/kyberswap';
@@ -11,13 +11,13 @@ import * as paraswap from './adapters/paraswap';
 
 // import * as unidex from "./adapters/unidex" - disabled, their api is broken
 // import * as airswap from './adapters/airswap' cors
-import * as odos from './adapters/odos';
+// import * as odos from './adapters/odos';
 // import * as yieldyak from './adapters/yieldyak';
 // import * as llamazip from './adapters/llamazip';
 // import * as krystal from './adapters/krystal'
 import * as matchaGasless from './adapters/0xGasless';
 
-export const adapters = [matcha, cowswap, paraswap, kyberswap, odos, matchaGasless];
+export const adapters = [matcha, cowswap, paraswap, kyberswap, inch, matchaGasless];
 
 export const inifiniteApprovalAllowed = [matcha.name, cowswap.name, matchaGasless.name];
 
