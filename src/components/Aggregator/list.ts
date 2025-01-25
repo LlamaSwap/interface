5c78b9b78b429a7c95dde96c69503db2ee6858af
vrtnd <vrotend@proton.me>
2025-01-25 14:43:04 +0300

    rmv odos

--- src/components/Aggregator/list.ts
+++ src/components/Aggregator/list.ts
@@ -11,13 +11,13 @@ import * as paraswap from './adapters/paraswap';
 
 // import * as unidex from "./adapters/unidex" - disabled, their api is broken
 // import * as airswap from './adapters/airswap' cors
-import * as odos from './adapters/odos';
+// import * as odos from './adapters/odos';
 // import * as yieldyak from './adapters/yieldyak';
 // import * as llamazip from './adapters/llamazip';
 // import * as krystal from './adapters/krystal'
 import * as matchaGasless from './adapters/0xGasless';
 
-export const adapters = [matcha, cowswap, paraswap, kyberswap, inch, matchaGasless, odos];
+export const adapters = [matcha, cowswap, paraswap, kyberswap, inch, matchaGasless];
 
 export const inifiniteApprovalAllowed = [matcha.name, cowswap.name, matchaGasless.name];
 
