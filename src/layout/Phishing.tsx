732ea4d608d0dbaec2805629189216f7f3a7ff0b
0xngmi <0xngmi@protonmail.com>
2025-04-16 13:12:25 +0100

    remove odos airdrop alert

--- src/layout/Phishing.tsx
+++ src/layout/Phishing.tsx
@@ -1,22 +1,6 @@
-import { CheckCircleIcon } from '@chakra-ui/icons';
 import { Alert, AlertIcon } from '@chakra-ui/react';
-import { useAccount } from 'wagmi';
-import { useGetEligibleAirdrops } from '~/queries/useGetEligibleAirdrops';
 
 const Phishing = () => {
-	const { address } = useAccount();
-
-	const { data: eligibleAirdrops } = useGetEligibleAirdrops({ address });
-
-	if (eligibleAirdrops) {
-		return (
-			<Alert status="success" justifyContent={'center'} fontWeight="bold" display={['none', 'none', 'flex', 'flex']}>
-				<CheckCircleIcon mr="4px" />
-				Congrats! You are eligible to claim {eligibleAirdrops} ODOS
-			</Alert>
-		);
-	}
-
 	return (
 		<Alert status="warning" justifyContent={'center'} fontWeight="bold" display={['none', 'none', 'flex', 'flex']}>
 			<AlertIcon mr="4px" />
