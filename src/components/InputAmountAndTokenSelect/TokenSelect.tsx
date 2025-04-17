46c82465937495f568c0f727334079dfecc80f26
mintdart <96025197+mintdart@users.noreply.github.com>
2025-04-17 04:31:13 -0700

    fix

--- src/components/InputAmountAndTokenSelect/TokenSelect.tsx
+++ src/components/InputAmountAndTokenSelect/TokenSelect.tsx
@@ -243,6 +243,9 @@ const SelectModal = ({ dialogState, data, onTokenSelect, selectedChain, isLoadin
 	const topHeight =
 		(topTokens.length > 0 ? 80 : 0) + (filteredBalances.length > 0 ? 8 + 36 + filteredBalances.length * 56 : 0) + 36;
 
+
+
+
 	return (
 		<>
 			<Dialog state={dialogState} backdropProps={{ className: 'dialog-backdrop' }}>
@@ -316,15 +319,17 @@ const SelectModal = ({ dialogState, data, onTokenSelect, selectedChain, isLoadin
 										))}
 									</>
 								) : null}
-								<ListHeader>
-									<svg viewBox="0 0 18 17" fill="none" strokeWidth="8" width={16} height={16}>
-										<path
-											d="M8.80054 0.829883L10.4189 4.09404C10.5406 4.33988 10.7756 4.50989 11.0481 4.54906L14.7838 5.08902C15.4688 5.18818 15.7422 6.0282 15.2464 6.50987L12.5456 9.13071C12.3481 9.32238 12.258 9.5982 12.3047 9.86904L12.9221 13.4557C13.0471 14.1832 12.283 14.7382 11.628 14.3957L8.38805 12.6999C8.14471 12.5724 7.85469 12.5724 7.61219 12.6999L4.37468 14.394C3.71885 14.7374 2.95216 14.1815 3.07799 13.4524L3.69556 9.86904C3.74223 9.5982 3.65218 9.32238 3.45468 9.13071L0.753871 6.50987C0.257205 6.0282 0.530481 5.18818 1.21631 5.08902L4.95217 4.54906C5.22384 4.50989 5.45885 4.33988 5.58135 4.09404L7.19969 0.829883C7.52553 0.167383 8.47221 0.167383 8.80054 0.829883Z"
-											fill="currentColor"
-										></path>
-									</svg>
-									<span>Tokens by 24H volume</span>
-								</ListHeader>
+								{rowVirtualizer.getVirtualItems().length > 0 ? (
+									<ListHeader>
+										<svg viewBox="0 0 18 17" fill="none" strokeWidth="8" width={16} height={16}>
+											<path
+												d="M8.80054 0.829883L10.4189 4.09404C10.5406 4.33988 10.7756 4.50989 11.0481 4.54906L14.7838 5.08902C15.4688 5.18818 15.7422 6.0282 15.2464 6.50987L12.5456 9.13071C12.3481 9.32238 12.258 9.5982 12.3047 9.86904L12.9221 13.4557C13.0471 14.1832 12.283 14.7382 11.628 14.3957L8.38805 12.6999C8.14471 12.5724 7.85469 12.5724 7.61219 12.6999L4.37468 14.394C3.71885 14.7374 2.95216 14.1815 3.07799 13.4524L3.69556 9.86904C3.74223 9.5982 3.65218 9.32238 3.45468 9.13071L0.753871 6.50987C0.257205 6.0282 0.530481 5.18818 1.21631 5.08902L4.95217 4.54906C5.22384 4.50989 5.45885 4.33988 5.58135 4.09404L7.19969 0.829883C7.52553 0.167383 8.47221 0.167383 8.80054 0.829883Z"
+												fill="currentColor"
+											></path>
+										</svg>
+										<span>Tokens by 24H volume</span>
+									</ListHeader>
+								) : null}
 								{rowVirtualizer.getVirtualItems().map((virtualRow) => (
 									<Row
 										token={filteredTokenList[virtualRow.index]}
@@ -377,9 +382,10 @@ export const TokenSelect = ({
 	// saved tokens list
 	const savedTokens = useGetSavedTokens(selectedChain?.id);
 
-	const { tokensInChain, topTokens, tokensWithBalances } = useMemo(() => {
-		const tokensWithBalances: Array<IToken> = [];
-		const tokensWithNoprice: Array<IToken> = [];
+	const { tokensInChain, topTokens, tokensWithBalances, topTokensWithBalances } = useMemo(() => {
+		const tokensWithBalances: Record<string, IToken> = {};
+		const tokensWithNoprice: Record<string, IToken> = {};
+		const topTokensWithBalances = new Set<string>();
 		const totalHoldingsInUsd = Object.values(tokenBalances ?? {}).reduce(
 			(acc, curr) => (acc += curr.balanceUSD ?? 0),
 			0
@@ -397,17 +403,18 @@ export const TokenSelect = ({
 				const balanceUSD = tokenBalances?.[t.address]?.balanceUSD ?? 0;
 
 				if (amount && balanceUSD && (balanceUSD >= 10 || balanceUSD >= totalHoldingsInUsd * 0.01)) {
-					tokensWithBalances.push({
+					topTokensWithBalances.add(t.address);
+					tokensWithBalances[t.address] = {
 						...t,
 						amount: tokenBalances?.[t.address]?.amount ?? 0,
 						balanceUSD: tokenBalances?.[t.address]?.balanceUSD ?? 0
-					});
+					};
 				} else {
-					tokensWithNoprice.push({
+					tokensWithNoprice[t.address] = {
 						...t,
 						amount: tokenBalances?.[t.address]?.amount ?? 0,
 						balanceUSD: tokenBalances?.[t.address]?.balanceUSD ?? 0
-					});
+					};
 				}
 			}
 		}
@@ -425,9 +432,10 @@ export const TokenSelect = ({
 				: [];
 
 		return {
-			tokensInChain: Object.values(tokensInChain).concat(tokensWithNoprice),
+			tokensInChain: Object.values({ ...tokensInChain, ...tokensWithNoprice }),
 			topTokens,
-			tokensWithBalances: tokensWithBalances.sort((a, b) => (b.balanceUSD ?? 0) - (a.balanceUSD ?? 0))
+			tokensWithBalances: Object.values(tokensWithBalances).sort((a, b) => (b.balanceUSD ?? 0) - (a.balanceUSD ?? 0)),
+			topTokensWithBalances
 		};
 	}, [chainTokenList, selectedChain?.id, tokenBalances, savedTokens, type]);
 
@@ -435,7 +443,7 @@ export const TokenSelect = ({
 		if (type === 'amountIn') {
 			return {
 				tokens: tokensInChain.filter(
-					({ address }) => address !== finalSelectedToToken?.address && !tokenBalances?.[address]
+					({ address }) => address !== finalSelectedToToken?.address && !topTokensWithBalances.has(address)
 				),
 				token: finalSelectedFromToken
 			};
@@ -443,11 +451,12 @@ export const TokenSelect = ({
 
 		return {
 			tokens: tokensInChain.filter(
-				({ address }) => address !== finalSelectedFromToken?.address && !tokenBalances?.[address]
+				({ address }) => address !== finalSelectedFromToken?.address && !topTokensWithBalances.has(address)
 			),
 			token: finalSelectedToToken
 		};
 	}, [tokensInChain, finalSelectedFromToken, finalSelectedToToken]);
+
 
 	const isLoading = type === 'amountIn' ? fetchingFromToken : fetchingToToken;
 
