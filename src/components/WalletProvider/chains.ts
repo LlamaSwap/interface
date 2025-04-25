8fbab651b05bb94981fac85c6ff138b8ecccba17
mintdart <96025197+mintdart@users.noreply.github.com>
2025-04-25 06:56:03 -0700

    update

--- src/components/WalletProvider/chains.ts
+++ src/components/WalletProvider/chains.ts
@@ -5,7 +5,6 @@ import * as wagmiChains from 'viem/chains';
 
 const okx = {
 	...wagmiChains.okc,
-	name: 'OKTChain',
 	network: 'okexchain',
 	iconUrl: chainIconUrl('oktchain'),
 	iconBackground: '#000'
@@ -13,7 +12,6 @@ const okx = {
 
 const binance = {
 	...wagmiChains.bsc,
-	name: 'Binance Smart Chain',
 	network: 'bsc',
 	iconUrl: chainIconUrl('binance'),
 	iconBackground: '#000'
@@ -21,7 +19,6 @@ const binance = {
 
 const boba = {
 	...wagmiChains.boba,
-	name: 'Boba',
 	network: 'boba',
 	iconUrl: chainIconUrl('boba'),
 	iconBackground: '#000'
@@ -29,7 +26,6 @@ const boba = {
 
 const harmony = {
 	...wagmiChains.harmonyOne,
-	name: 'Harmony',
 	network: 'harmony',
 	iconUrl: chainIconUrl('harmony'),
 	iconBackground: '#000'
@@ -60,7 +56,6 @@ const heco = {
 
 const velas = {
 	...wagmiChains.velas,
-	name: 'Velas',
 	network: 'velas',
 	iconUrl: chainIconUrl('velas'),
 	iconBackground: '#000'
@@ -88,7 +83,6 @@ const oasis = {
 
 const moonbeam = {
 	...wagmiChains.moonbeam,
-	name: 'Moonbeam',
 	network: 'moonbeam',
 	iconUrl: chainIconUrl('moonbeam'),
 	iconBackground: '#000'
@@ -96,7 +90,6 @@ const moonbeam = {
 
 const fuse = {
 	...wagmiChains.fuse,
-	name: 'Fuse',
 	network: 'fuse',
 	iconUrl: chainIconUrl('fuse'),
 	iconBackground: '#000'
@@ -104,7 +97,6 @@ const fuse = {
 
 const moonriver = {
 	...wagmiChains.moonriver,
-	name: 'MoonRiver',
 	network: 'moonriver',
 	iconUrl: chainIconUrl('moonriver'),
 	iconBackground: '#000'
@@ -112,28 +104,24 @@ const moonriver = {
 
 const cronos = {
 	...wagmiChains.cronos,
-	name: 'Cronos',
 	network: 'cronos',
 	iconUrl: chainIconUrl('cronos'),
 	iconBackground: '#000'
 };
 const celo = {
 	...wagmiChains.celo,
-	name: 'Celo',
 	network: 'celo',
 	iconUrl: chainIconUrl('celo'),
 	iconBackground: '#000'
 };
 const aurora = {
 	...wagmiChains.aurora,
-	name: 'Aurora',
 	network: 'aurora',
 	iconUrl: chainIconUrl('aurora'),
 	iconBackground: '#000'
 };
 const avax = {
 	...wagmiChains.avalanche,
-	name: 'AVAX',
 	network: 'avax',
 	iconUrl: chainIconUrl('avalanche'),
 	iconBackground: '#000'
@@ -141,14 +129,12 @@ const avax = {
 
 const klaytn = {
 	...wagmiChains.klaytn,
-	name: 'Klaytn',
 	network: 'klaytn',
 	iconUrl: chainIconUrl('klaytn'),
 	iconBackground: '#000'
 };
 const fantom = {
 	...wagmiChains.fantom,
-	name: 'Fantom Opera',
 	network: 'fantom',
 	iconUrl: chainIconUrl('fantom'),
 	iconBackground: '#000'
@@ -156,14 +142,12 @@ const fantom = {
 
 const gnosis = {
 	...wagmiChains.gnosis,
-	name: 'Gnosis',
 	network: 'gnosis',
 	iconUrl: chainIconUrl('gnosis'),
 	iconBackground: '#000'
 };
 const polygon = {
 	...wagmiChains.polygon,
-	name: 'Polygon',
 	network: 'polygon',
 	iconUrl: chainIconUrl('polygon'),
 	iconBackground: '#000'
@@ -171,7 +155,6 @@ const polygon = {
 
 const canto = {
 	...wagmiChains.canto,
-	name: 'Canto',
 	network: 'Canto',
 	iconUrl: chainIconUrl('canto'),
 	iconBackground: '#000'
@@ -179,7 +162,6 @@ const canto = {
 
 const arbitrum = {
 	...wagmiChains.arbitrum,
-	name: 'Arbitrum',
 	network: 'arbitrum',
 	iconUrl: chainIconUrl('arbitrum'),
 	iconBackground: '#000'
@@ -187,7 +169,6 @@ const arbitrum = {
 
 const ethereum = {
 	...wagmiChains.mainnet,
-	name: 'Ethereum',
 	network: 'ethereum',
 	iconUrl: chainIconUrl('ethereum'),
 	iconBackground: '#000'
@@ -195,7 +176,6 @@ const ethereum = {
 
 const optimism = {
 	...wagmiChains.optimism,
-	name: 'Optimism',
 	network: 'optimism',
 	iconUrl: chainIconUrl('optimism'),
 	iconBackground: '#000'
@@ -203,7 +183,6 @@ const optimism = {
 
 const zksync = {
 	...wagmiChains.zksync,
-	name: 'zkSync Era',
 	network: 'zksync',
 	iconUrl: chainIconUrl('zksync era'),
 	iconBackground: '#000'
@@ -231,7 +210,6 @@ const ontology = {
 
 const polygonZKEvm = {
 	...wagmiChains.polygonZkEvm,
-	name: 'Polygon zkEVM',
 	network: 'polygonzkevm',
 	iconUrl: chainIconUrl('Polygon zkEVM'),
 	iconBackground: '#000'
@@ -239,7 +217,6 @@ const polygonZKEvm = {
 
 const kava = {
 	...wagmiChains.kava,
-	name: 'Kava',
 	network: 'kava',
 	iconUrl: chainIconUrl('Kava'),
 	iconBackground: '#000'
@@ -247,7 +224,6 @@ const kava = {
 
 const metis = {
 	...wagmiChains.metis,
-	name: 'Metis',
 	network: 'metis',
 	iconUrl: chainIconUrl('metis'),
 	iconBackground: '#000'
@@ -255,7 +231,6 @@ const metis = {
 
 const pulse = {
 	...wagmiChains.pulsechain,
-	name: 'PulseChain',
 	network: 'pulse',
 	iconUrl: chainIconUrl('Pulse'),
 	iconBackground: '#000'
@@ -263,7 +238,6 @@ const pulse = {
 
 const base = {
 	...wagmiChains.base,
-	name: 'Base',
 	network: 'base',
 	iconUrl: chainIconUrl('Base'),
 	iconBackground: '#000'
@@ -271,7 +245,6 @@ const base = {
 
 const linea = {
 	...wagmiChains.linea,
-	name: 'Linea',
 	network: 'linea',
 	iconUrl: chainIconUrl('Linea'),
 	iconBackground: '#000'
@@ -279,7 +252,6 @@ const linea = {
 
 const scroll = {
 	...wagmiChains.scroll,
-	name: 'Scroll',
 	network: 'scroll',
 	iconUrl: chainIconUrl('Scroll'),
 	iconBackground: '#000'
@@ -287,7 +259,6 @@ const scroll = {
 
 const sonic = {
 	...wagmiChains.sonic,
-	name: 'Sonic',
 	network: 'sonic',
 	iconUrl: chainIconUrl('sonic'),
 	iconBackground: '#000'
