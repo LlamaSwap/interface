5fb5a2fce76e3581845ced6f710912835885c6d0
mintdart <96025197+mintdart@users.noreply.github.com>
2025-06-22 05:19:04 +0900

    use allowance holder api for 0x v2 eip5792 txs

--- src/components/Aggregator/adapters/0xV2.ts
+++ src/components/Aggregator/adapters/0xV2.ts
@@ -1,4 +1,4 @@
-import { numberToHex, size, zeroAddress, concat} from 'viem';
+import { numberToHex, size, zeroAddress, concat } from 'viem';
 import { sendTx } from '../utils/sendTx';
 import { getTxs } from '../utils/getTxs';
 
@@ -25,10 +25,10 @@ export const chainToId = {
 const nativeToken = '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE';
 const feeCollectorAddress = '0x9Ab6164976514F1178E2BB4219DA8700c9D96E9A';
 const permit2Address = '0x000000000022d473030f116ddee9f6b43ac78ba3';
+const allowanceHolderAddress = '0x0000000000001fF3684f28c67538d4D072C22734';
 
 export async function getQuote(chain: string, from: string, to: string, amount: string, extra) {
 	// amount should include decimals
-
 	const tokenFrom = from === zeroAddress ? nativeToken : from;
 	const tokenTo = to === zeroAddress ? nativeToken : to;
 
@@ -42,6 +42,50 @@ export async function getQuote(chain: string, from: string, to: string, amount: 
 
 	// only expects integer
 	const slippage = (extra.slippage * 100) | 0;
+
+	if (extra.isAtomicTxsSupported) {
+		const data = await fetch(
+			`https://api.0x.org/swap/allowance-holder/quote?chainId=${chainToId[chain]}&buyToken=${tokenTo}&${amountParam}&sellToken=${tokenFrom}&slippageBps=${slippage}&taker=${taker}&tradeSurplusRecipient=${feeCollectorAddress}`,
+			{
+				headers: {
+					'0x-api-key': process.env.OX_API_KEY as string,
+					'0x-version': 'v2'
+				}
+			}
+		).then(async (r) => {
+			if (r.status !== 200) {
+				// throw new Error('Failed to fetch');
+				return null;
+			}
+
+			const data = await r.json();
+
+			return data;
+		}).catch((e) => {
+			console.log(e);
+			return null;
+		});
+
+		// fetch permit2 quote if allowance holder quote is not available
+		if (data) {
+			if (
+				data?.issues?.allowance?.spender &&
+				data.issues.allowance.spender.toLowerCase() !== allowanceHolderAddress.toLowerCase()
+			) {
+				throw new Error(`Approval address does not match`);
+			}
+
+			return {
+				amountReturned: data?.buyAmount || 0,
+				amountIn: data?.sellAmount || 0,
+				tokenApprovalAddress: allowanceHolderAddress,
+				estimatedGas: data.transaction.gas,
+				rawQuote: { ...data, gasLimit: data.transaction.gas },
+				isSignatureNeededForSwap: false,
+				logo: 'https://www.gitbook.com/cdn-cgi/image/width=40,height=40,fit=contain,dpr=2,format=auto/https%3A%2F%2F1690203644-files.gitbook.io%2F~%2Ffiles%2Fv0%2Fb%2Fgitbook-x-prod.appspot.com%2Fo%2Fspaces%252FKX9pG8rH3DbKDOvV7di7%252Ficon%252F1nKfBhLbPxd2KuXchHET%252F0x%2520logo.png%3Falt%3Dmedia%26token%3D25a85a3e-7f72-47ea-a8b2-e28c0d24074b'
+			};
+		}
+	}
 
 	const data = await fetch(
 		`https://api.0x.org/swap/permit2/quote?chainId=${chainToId[chain]}&buyToken=${tokenTo}&${amountParam}&sellToken=${tokenFrom}&slippageBps=${slippage}&taker=${taker}&tradeSurplusRecipient=${feeCollectorAddress}`,
@@ -81,14 +125,14 @@ export async function getQuote(chain: string, from: string, to: string, amount: 
 
 export async function signatureForSwap({ rawQuote, signTypedDataAsync }) {
 	const signature = await signTypedDataAsync(rawQuote.permit2.eip712).catch((err) => {
-		console.log(err)
+		console.log(err);
 	});
 	return signature;
 }
 
 export async function swap({ tokens, fromAmount, fromAddress, rawQuote, signature, eip5792 }) {
-	if (!signature) {
-		throw { reason: 'Signature is required' }
+	if (rawQuote.isSignatureNeededForSwap && tokens.fromToken.address !== zeroAddress && !signature) {
+		throw { reason: 'Signature is required' };
 	}
 
 	const signatureLengthInHex = numberToHex(size(signature), {
